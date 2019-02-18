6532e5d61f461c51e730e497df3f3ebd113ee64a
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-02-18 16:02:08 +0100

    Make a general script

--- compiling_delft3d/compile_following_maxime_master.sh
+++ compiling_delft3d/compile_following_maxime_master.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-
-# set the following variables:
-# - (temporary) working folder (i.e. for downloading, etc). 
-MY_WORKING_FOLDER="/scratch-shared/edwinkle/compiling_delft3d/"
-# - svn (deltares) delft3d user name 
-SVN_USER_NAME="hsutanudjajacchms99"
-# - (deltares) delft3d svn site
-SVN_SRC_SITE=
-# - folder where delft3d version from the (deltares) delft3d svn site will be saved (relative to MY_WORKING_FOLDER)
-SVN_SRC_FOLDER=
-# - folder where delft3d version from marcio is located
-OWN_SRC_FOLDER=
-# - target folder where delft3d will be installed
-TARGET_INST_FOLDER=
-
-
-# set, make and print (temporary) working folder
-mkdir -p ${MY_WORKING_FOLDER}
-cd ${MY_WORKING_FOLDER}
-pwd
-
-
-# obtain the delft3d from the Deltares site
-svn 
-
-svn --username hsutanudjajacchms99 checkout https://svn.oss.deltares.nl/repos/delft3d/tags/delft3d4/7545 delft3d_repository_tag-7545
-
-# replace and 
-
-
 # load all required modules
 module purge
 module load surfsara
@@ -38,7 +8,6 @@ module load intel/2017b
 module load netCDF/4.4.1.1-intel-2017b
 module load netCDF-Fortran/4.4.4-intel-2017b
 
-# set the correct variables
 export FC=mpiifort
 export F77=mpiifort
 export CC=mpiicc
