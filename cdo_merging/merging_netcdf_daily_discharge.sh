275070c1d3e71f0ebc203ecbd7a9eb9d8001e359
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-04-23 10:45:03 +0200

    remerging using cat

--- cdo_merging/merging_netcdf_daily_discharge.sh
+++ cdo_merging/merging_netcdf_daily_discharge.sh
@@ -8,8 +8,8 @@ mkdir -p ${TARGET_FOLDER}
 STA_YEAR=1958
 END_YEAR=1960
 SOURCE_FOLDER=${MAIN_SOURCE_FOLDER}/begin_from_1958/
-cdo -L -mergetime ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
-cdo -L -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
+cdo -L -f nc4 -cat ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
+cdo -L -f nc4 -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
 rm ${TARGET_FOLDER}/temp.nc
 unset STA_YEAR
 unset END_YEAR
@@ -17,8 +17,8 @@ unset END_YEAR
 STA_YEAR=1961
 END_YEAR=1977
 SOURCE_FOLDER=${MAIN_SOURCE_FOLDER}/continue_from_1961/
-cdo -L -mergetime ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
-cdo -L -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
+cdo -L -f nc4 -cat ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
+cdo -L -f nc4 -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
 rm ${TARGET_FOLDER}/temp.nc
 unset STA_YEAR
 unset END_YEAR
@@ -26,8 +26,8 @@ unset END_YEAR
 STA_YEAR=1978
 END_YEAR=1997
 SOURCE_FOLDER=${MAIN_SOURCE_FOLDER}/continue_from_1978/
-cdo -L -mergetime ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
-cdo -L -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
+cdo -L -f nc4 -cat ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
+cdo -L -f nc4 -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
 rm ${TARGET_FOLDER}/temp.nc
 unset STA_YEAR
 unset END_YEAR
@@ -35,8 +35,8 @@ unset END_YEAR
 STA_YEAR=1998
 END_YEAR=2013
 SOURCE_FOLDER=${MAIN_SOURCE_FOLDER}/continue_from_1998/
-cdo -L -mergetime ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
-cdo -L -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
+cdo -L -f nc4 -cat ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
+cdo -L -f nc4 -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
 rm ${TARGET_FOLDER}/temp.nc
 unset STA_YEAR
 unset END_YEAR
@@ -44,8 +44,8 @@ unset END_YEAR
 STA_YEAR=2014
 END_YEAR=2015
 SOURCE_FOLDER=${MAIN_SOURCE_FOLDER}/continue_from_2014/
-cdo -L -mergetime ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
-cdo -L -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
+cdo -L -f nc4 -cat ${SOURCE_FOLDER}/global/netcdf/discharge_dailyTot_output_*.nc ${TARGET_FOLDER}/temp.nc
+cdo -L -f nc4 -selyear,${STA_YEAR}/${END_YEAR} ${TARGET_FOLDER}/temp.nc ${TARGET_FOLDER}/discharge_dailyTot_output_${STA_YEAR}-01-01_to_${END_YEAR}-12-31.nc
 rm ${TARGET_FOLDER}/temp.nc
 unset STA_YEAR
 unset END_YEAR
