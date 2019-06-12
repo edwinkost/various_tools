5183c55a3b7f62e6ef8fc952144148ca2f73d74c
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-06-12 14:47:28 +0200

    merging aqueduct fossil groundwater abstraction - rcp2p6

---
+++ merge_netcdf/bash_script_for_merging_aqueduct_fossil_groundwater_abstraction_rcp2p6.sh
@@ -0,0 +1,21 @@
+
+set -x
+
+GCM_CODE="gfdl-esm2m"
+python merging_aqueduct_fossil_groundwater_abstraction.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_${GCM_CODE}/no_correction/non-natural/" 2006,2011,2017,2048,2079 2099 "/projects/0/dfguu/users/edwin/pcr-globwb-aqueduct/rcp2p6/2006-2099/${GCM_CODE}/" &
+
+GCM_CODE="hadgem2-es"
+python merging_aqueduct_fossil_groundwater_abstraction.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_${GCM_CODE}/no_correction/non-natural/" 2006,2037,2043,2074 2099 "/projects/0/dfguu/users/edwin/pcr-globwb-aqueduct/rcp2p6/2006-2099/${GCM_CODE}/" &
+
+GCM_CODE="ipsl-cm5a-lr"
+python merging_aqueduct_fossil_groundwater_abstraction.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_${GCM_CODE}/no_correction/non-natural/" 2006,2037,2051,2074 2099 "/projects/0/dfguu/users/edwin/pcr-globwb-aqueduct/rcp2p6/2006-2099/${GCM_CODE}/" &
+
+GCM_CODE="miroc-esm-chem"
+python merging_aqueduct_fossil_groundwater_abstraction.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_${GCM_CODE}/no_correction/non-natural/" 2006,2037,2050,2073 2099 "/projects/0/dfguu/users/edwin/pcr-globwb-aqueduct/rcp2p6/2006-2099/${GCM_CODE}/" &
+
+GCM_CODE="noresm1-m"
+python merging_aqueduct_fossil_groundwater_abstraction.py "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_${GCM_CODE}/no_correction/non-natural/" 2006,2037,2051,2075,2099 2099 "/projects/0/dfguu/users/edwin/pcr-globwb-aqueduct/rcp2p6/2006-2099/${GCM_CODE}/" &
+
+wait
+
+set +x
