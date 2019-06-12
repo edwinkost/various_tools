237f3080a0f99bd54e5be9d110bf000f3f3d154b
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-06-12 13:00:33 +0200

    merging aqueduct fossil groundwater abstraction

--- merge_netcdf/merging_aqueduct_fossil_groundwater_abstraction.py
+++ merge_netcdf/merging_aqueduct_fossil_groundwater_abstraction.py
@@ -9,4 +9,8 @@ print(directory)
 
 #~ example: python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_watch_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwin/test_merging/ outAnnuaTotNC 1958-12-31 1958-12-31 fossilGroundwaterAbstraction NETCDF4 True 1 Global
 
+main_folder = 
+sub_folders =
+ 
 
+for 
