251ba0f12ed099e4a095d763c285195068b3ace2
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-07-02 04:52:09 +0200

    finalize the script to compile cdo on eejit

--- compiling_cdo/compile_cdo_on_fleet.sh
+++ compiling_cdo/compile_cdo_on_fleet.sh
@@ -1,5 +1,7 @@
 # source http://www.studytrails.com/blog/install-climate-data-operator-cdo-with-netcdf-grib2-and-hdf5-support/
 
+set -x
+
 # activate g++
 scl enable devtoolset-8 bash
 
@@ -53,12 +55,35 @@ make -j 6
 make check -j 6
 make install
 
-#~ # download and install Jasper using ./configure –prefix=/opt/cdo-install  CFLAGS=-fPIC ; ‘make’, ‘make check’ and ‘make install’
-#~ # NOT NEEDED
+# download and install Jasper using ./configure –prefix=/opt/cdo-install  CFLAGS=-fPIC ; ‘make’, ‘make check’ and ‘make install’
+cd ${SOURCE_FOLDER}
+wget https://www.ece.uvic.ca/~frodo/jasper/software/jasper-2.0.14.tar.gz
+tar -xvf jasper-2.0.14.tar.gz
+cd jasper-2.0.14
+./configure --prefix=${TARGET_FOLDER}  CFLAGS=-fPIC
+make -j 6
+make check -j 6
+make install
 
-#~ # download and install grib using ./configure –prefix=/opt/cdo-install CFLAGS=-fPIC  –with-netcdf=/opt/cdo-install –with-jasper=/opt/cdo-install ; ‘make’, ‘make check’ and ‘make install’
+# download and install grib using ./configure –prefix=/opt/cdo-install CFLAGS=-fPIC  –with-netcdf=/opt/cdo-install –with-jasper=/opt/cdo-install ; ‘make’, ‘make check’ and ‘make install’
+cd ${SOURCE_FOLDER}
+wget https://confluence.ecmwf.int/download/attachments/3473437/grib_api-1.28.0-Source.tar.gz
+tar -xvf grib_api-1.28.0-Source.tar.gz
+cd grib_api-1.28.0-Source
+./configure --prefix=${TARGET_FOLDER} CFLAGS=-fPIC  --with-netcdf=${TARGET_FOLDER} --with-jasper=${TARGET_FOLDER}
+make -j 6
+make check -j 6
+make install
 
-# WE SHOULD ALSO INCLUDE UDUNITS2?
+# WE SHOULD ALSO INCLUDE UDUNITS2
+cd ${SOURCE_FOLDER}
+wget ftp://ftp.unidata.ucar.edu/pub/udunits/udunits-2.2.26.tar.gz
+tar -xvf udunits-2.2.26.tar.gz
+cd udunits-2.2.26
+./configure --prefix=${TARGET_FOLDER} --disable-shared
+make -j 6
+make check -j 6
+make install
 
 # download and install cdo using ./configure –prefix=/opt/cdo-install CFLAGS=-fPIC  –with-netcdf=/opt/cdo-install –with-jasper=/opt/cdo-install –with-hdf5=/opt/cdo-install  –with-grib_api=/opt/cdo-install ; ‘make’, ‘make check’ and ‘make install’
 cd ${SOURCE_FOLDER}
@@ -66,6 +91,9 @@ wget https://code.mpimet.mpg.de/attachments/download/19299/cdo-1.9.6.tar.gz
 tar -xvf cdo-1.9.6.tar.gz
 cd cdo-1.9.6
 ./configure --prefix=${TARGET_FOLDER} CFLAGS=-fPIC --with-netcdf=${TARGET_FOLDER} --with-hdf5=${TARGET_FOLDER}
+./configure --prefix=${TARGET_FOLDER} CFLAGS=-fPIC --with-netcdf=${TARGET_FOLDER} --with-jasper=${TARGET_FOLDER} --with-hdf5=${TARGET_FOLDER} --with-grib_api=${TARGET_FOLDER} --with-udunits2=${TARGET_FOLDER}
 make -j 6
 make check -j 6
 make install
+
+set +x
