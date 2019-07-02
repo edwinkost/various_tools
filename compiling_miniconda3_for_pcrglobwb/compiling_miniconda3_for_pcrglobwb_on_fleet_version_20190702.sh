ca5828c3fbe147c689f739cc60d9a29da9c03295
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-07-02 14:40:27 +0200

    Add a script to install minconda3 etc on fleet

--- compiling_miniconda3_for_pcrglobwb/compiling_miniconda3_for_pcrglobwb_on_fleet_version_20190702.sh
+++ compiling_miniconda3_for_pcrglobwb/compiling_miniconda3_for_pcrglobwb_on_fleet_version_20190702.sh
@@ -1,4 +1,6 @@
 
+set -x
+
 # get and install miniconda, set the directory to /home/sutan101/opt/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
@@ -13,4 +15,6 @@ conda install numpy udunits2 proj4 zlib hdf4 hdf5 netcdf4 h5py gdal=2.3.0 basema
 PCRASTER=/opt/pcraster/pcraster-4.2.1
 export PATH=$PCRASTER/bin:$PATH
 export PYTHONPATH=$PCRASTER/python:$PYTHONPATH
+
+set +x
 
