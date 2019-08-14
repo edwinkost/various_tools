c2639e0df8a4ebf8190b65ba45cbd6ec7d166402
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-08-14 03:10:29 +0200

    copying hydroworld to /quanta1/home/hydrowld/data/hydroworld/

--- rsync_various/rsync_hydroworld_from_cartesius_to_eejit.sh
+++ rsync_various/rsync_hydroworld_from_cartesius_to_eejit.sh
@@ -2,17 +2,32 @@
 
 set -x
 
+# NOTE: It is recommended for NOT USING hydrowld account of cartesius. 
+
 SOURCE="/projects/0/dfguu/data/hydroworld/"
-TARGET="eejit_hydrowld_via_speedy:/scratch/depfg/hydrowld/data/hydroworld/"
+
+# - to hydrowld home
+TARGET="eejit_hydrowld_via_speedy:/quanta1/home/hydrowld/data/hydroworld/"
+
+#~ # - to /scratch (disk is fast, but it is often failed)
+#~ TARGET="eejit_hydrowld_via_speedy:/scratch/depfg/hydrowld/data/hydroworld/"
+
+
+# the following options should also include copying real files and folders of symlinks
+OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress --copy-links"
+
+# the following options should make symlinks broken
+#~ OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress"
+
 
 # go to the source folder
 cd ${SOURCE}
 
 # rsync command
-rsync --recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress * ${TARGET}
+rsync ${OPTIONS} * ${TARGET}
 
 # rsync command - for final checking
-rsync --recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress * ${TARGET}
+rsync ${OPTIONS} * ${TARGET} 
 
 # go back to the original folder (where this script is excecuted)
 cd -
