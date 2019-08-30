5d7739c9db9609c1851a934d226e5ec413df2f04
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-08-30 14:26:44 +0200

    merging annuaTot

--- merge_netcdf/merging_various/mergingAnnuaTot.py
+++ merge_netcdf/merging_various/mergingAnnuaTot.py
@@ -15,4 +15,4 @@ for year in range(1958, 2015 + 1):
     fulldate = str(year) + "-12-31"
     cmd_line = "python merge_netcdf.py /scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/begin_from_1958/ /scratch-shared/edwin/test_merging/1958/ outAnnuaTotNC " + fulldate + " " + fulldate + " " + "totalEvaporation,precipitation,gwRecharge,totalRunoff,baseflow,desalinationAbstraction,surfaceWaterAbstraction,totalGroundwaterAbstraction,nonFossilGroundwaterAbstraction,fossilGroundwaterAbstraction,totalAbstraction,irrGrossDemand,nonIrrGrossDemand,totalGrossDemand,nonIrrWaterConsumption,nonIrrReturnFlow,runoff,actualET,irrPaddyWaterWithdrawal,irrigationWaterWithdrawal,domesticWaterWithdrawal,industryWaterWithdrawal,livestockWaterWithdrawal,precipitation_at_irrigation,netLqWaterToSoil_at_irrigation,evaporation_from_irrigation,transpiration_from_irrigation,referencePotET NETCDF4 True 1 Global"
     print(cmd_line)
-    #~ os.system(cmd)
+    os.system(cmd)
