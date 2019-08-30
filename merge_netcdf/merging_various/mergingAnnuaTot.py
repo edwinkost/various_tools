82f82a8340345c56c76c98c6996a40515589add6
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-08-30 15:30:32 +0200

    merging annuaTot

--- merge_netcdf/merging_various/mergingAnnuaTot.py
+++ merge_netcdf/merging_various/mergingAnnuaTot.py
@@ -4,8 +4,8 @@
 import os
 import sys
 
-pcrglobwb_output_folder = "/scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/begin_from_1958/"
-merged_output_folder    = "/scratch-shared/edwin/tmp/mergedAnnuaTot/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/begin_from_1958/"
+pcrglobwb_output_folder = "/scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_rens-hyde-irrigated_areas/begin_from_1958/"
+merged_output_folder    = "/scratch-shared/edwin/tmp/mergedAnnuaTot/pcrglobwb_for_land-subsidence-model_runs_version_201908XX//historical_non-natural_rens-hyde-irrigated_areas/begin_from_1958/"
 
 cmd_line = "mkdir -p " + merged_output_folder
 print(cmd_line)
@@ -13,6 +13,6 @@ os.system(cmd_line)
 
 for year in range(1958, 2015 + 1):
     fulldate = str(year) + "-12-31"
-    cmd_line = "python merge_netcdf.py /scratch-shared/edwin/pcrglobwb_for_land-subsidence-model_runs_version_201908XX/historical_non-natural_gmd-paper-irrigated_areas/begin_from_1958/ " + merged_output_folder + " outAnnuaTotNC " + fulldate + " " + fulldate + " " + "totalEvaporation,precipitation,gwRecharge,totalRunoff,baseflow,desalinationAbstraction,surfaceWaterAbstraction,totalGroundwaterAbstraction,nonFossilGroundwaterAbstraction,fossilGroundwaterAbstraction,totalAbstraction,irrGrossDemand,nonIrrGrossDemand,totalGrossDemand,nonIrrWaterConsumption,nonIrrReturnFlow,runoff,actualET,irrPaddyWaterWithdrawal,irrigationWaterWithdrawal,domesticWaterWithdrawal,industryWaterWithdrawal,livestockWaterWithdrawal,precipitation_at_irrigation,netLqWaterToSoil_at_irrigation,evaporation_from_irrigation,transpiration_from_irrigation,referencePotET NETCDF4 True 20 Global"
+    cmd_line = "python merge_netcdf.py " + pcrglobwb_output_folder + " " + merged_output_folder + " outAnnuaTotNC " + fulldate + " " + fulldate + " " + "totalEvaporation,precipitation,gwRecharge,totalRunoff,baseflow,desalinationAbstraction,surfaceWaterAbstraction,totalGroundwaterAbstraction,nonFossilGroundwaterAbstraction,fossilGroundwaterAbstraction,totalAbstraction,irrGrossDemand,nonIrrGrossDemand,totalGrossDemand,nonIrrWaterConsumption,nonIrrReturnFlow,runoff,actualET,irrPaddyWaterWithdrawal,irrigationWaterWithdrawal,domesticWaterWithdrawal,industryWaterWithdrawal,livestockWaterWithdrawal,precipitation_at_irrigation,netLqWaterToSoil_at_irrigation,evaporation_from_irrigation,transpiration_from_irrigation,referencePotET NETCDF4 True 20 Global"
     print(cmd_line)
     os.system(cmd_line)
