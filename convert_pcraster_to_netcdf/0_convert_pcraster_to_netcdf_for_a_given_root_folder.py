dcc78cc70840af5c177a39af7a2c1bdf80c55150
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-10-16 16:52:44 +0200

    star making tools for converting files

---
+++ convert_pcraster_to_netcdf/0_convert_pcraster_to_netcdf_for_a_given_root_folder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import pcraster as pcr
+
+# utility modules:
+import virtualOS as vos
+import outputNetcdf as out_nc
+
+
+def main():
+    
+    path = "/scratch/depfg/sutan101/pcrglobwb2_input_release/develop_complete_set_for_4tu_opendap/pcrglobwb2_input/"
+    
+    for root, dirs, files in os.walk(path):
+    for name in files:
+        print(name)
+    
+    print("Done!")                          
+                                        
+
+if __name__ == '__main__':
+    sys.exit(main())
