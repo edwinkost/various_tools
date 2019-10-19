203896cf54e8f9029ab378d389010e579fe18d05
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-10-19 22:23:46 +0200

    include copying pcraster map files

--- convert_pcraster_to_netcdf/0_convert_pcraster_to_netcdf_for_opendap_pcrglobwb2_input.py
+++ convert_pcraster_to_netcdf/0_convert_pcraster_to_netcdf_for_opendap_pcrglobwb2_input.py
@@ -9,7 +9,7 @@ import pcraster_to_netcdf as pcr2nc
 
 def main():
     
-    source_path = "/quanta1/home/hydrowld/data/hydroworld/pcrglobwb2_input_release/version_2019_10_beta_1/"
+    source_path = "/scratch/depfg/sutan101/data/pcrglobwb2_input_release/version_2019_10_beta_1/"
     
     target_path = "/scratch/depfg/sutan101/data/pcrglobwb2_input_release/develop_complete_set_for_opendap/"
     
@@ -35,14 +35,20 @@ def main():
             
             # get target file_name
             target_file_name = source_file_name.replace(source_path, target_path)
-            # - rename ".nc4" to "nc" (the standard extension of netcdf file is ".nc")
-            if target_file_name.endswith(".nc4"): target_file_name = target_file_name[:-1]
 
             # make sure that the output directory is ready
             target_directory = os.path.dirname(source_file_name).replace(source_path, target_path)
             if os.path.exists(target_directory) == False: os.makedirs(target_directory)
+
+            #~ # - go to the target directory - not necessary
+            #~ os.chdir(target_directory)
             
-            if target_file_name.endswith(".nc"):
+            if target_file_name.endswith(".nc") or target_file_name.endswith(".nc4"):
+
+                # for netcdf files
+
+                # - rename ".nc4" to "nc" (the standard extension of netcdf file is ".nc")
+                if target_file_name.endswith(".nc4"): target_file_name = target_file_name[:-1]
 
                 #~ # for netcdf files, just copy
                 #~ shutil.copy(source_file_name, target_file_name)
@@ -50,14 +56,20 @@ def main():
                 # for netcdf files, compress them using cdo (edwin prefers cdo as it includes 'history')
                 cmd_line = 'cdo -L -z zip -f nc4 -copy ' + source_file_name + " " + target_file_name
 
-                #~ # - alternative: using cdo
+                #~ # - alternative: using nco
                 #~ cmd_line = 'nccopy -k netCDF-4 -d1 -u ' + source_file_name + " " + target_file_name
 
                 print(cmd_line)
                 os.system(cmd_line)
             
             elif target_file_name.endswith(".map"):  
-                # for pcraster map files, convert them to netcdf 
+
+                # for pcraster map files
+                
+                # first copy them
+                shutil.copy(source_file_name, target_file_name)
+
+                # then, convert them to netcdf
                 target_file_name = target_file_name[:-4] + ".nc"
                 pcr2nc.convert_pcraster_to_netcdf(input_pcr_map_file = source_file_name,\
                                                   output_netcdf_file = target_file_name)
