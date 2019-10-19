d4361aad7074f54305b81094cce1ed78050740c2
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-10-20 00:35:56 +0200

    finalizing

--- convert_pcraster_to_netcdf/0_convert_pcraster_to_netcdf_for_opendap_pcrglobwb2_input.py
+++ convert_pcraster_to_netcdf/0_convert_pcraster_to_netcdf_for_opendap_pcrglobwb2_input.py
@@ -60,7 +60,7 @@ def main():
                 #~ cmd_line = 'cdo -L -z zip_1 -f nc4 -copy ' + source_file_name + " " + target_file_name
 
                 # - turn off compression, but we make sure that the format is nc4
-                cmd_line = 'cdo -L -z zip_0 -f nc4 -copy ' + source_file_name + " " + target_file_name
+                cmd_line = 'cdo -L -f nc4 -copy ' + source_file_name + " " + target_file_name
 
                 #~ # - alternative: using nco
                 #~ cmd_line = 'nccopy -k netCDF-4 -d1 -u ' + source_file_name + " " + target_file_name
@@ -86,10 +86,10 @@ def main():
                 
                 # for other files
                 
-                # just copy
-                msg = "copying " + source_file_name + " to " + target_file_name
-                print(msg)
-                shutil.copy(source_file_name, target_file_name)
+                #~ # just copy
+                #~ msg = "copying " + source_file_name + " to " + target_file_name
+                #~ print(msg)
+                #~ shutil.copy(source_file_name, target_file_name)
 
                 # skip
                 pass
