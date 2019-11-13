dccbc936979e3e34c264694e622dd540497b15a0
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-11-14 00:33:46 +0100

    initial conditions - 5min - 1960

---
+++ convert_pcraster_to_netcdf/set_units_for_initial_conidtions.sh
@@ -0,0 +1,65 @@
+
+MAINFOLDER="/scratch/depfg/sutan101/data/pcrglobwb2_input_release/version_2019_11_beta_extended/pcrglobwb2_input/global_05min/initialConditions/non-natural/1960/"
+
+set -x
+
+cd ${MAINFOLDER}
+
+mkdir nc_without_units
+cp -rvf *.nc nc_without_units
+
+cdo setunit,"m3 s-1"    nc_without_units/avgBaseflowLong_*-12-31.nc                        avgBaseflowLong_*-12-31.nc
+cdo setunit,"m3 s-1"    nc_without_units/avgDischargeLong_*-12-31.nc                       avgDischargeLong_*-12-31.nc
+cdo setunit,"m3 s-1"    nc_without_units/avgDischargeShort_*-12-31.nc                      avgDischargeShort_*-12-31.nc
+cdo setunit,"m3 s-1"    nc_without_units/avgLakeReservoirInflowShort_*-12-31.nc            avgLakeReservoirInflowShort_*-12-31.nc
+cdo setunit,"m3 s-1"    nc_without_units/avgLakeReservoirOutflowLong_*-12-31.nc            avgLakeReservoirOutflowLong_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/avgNonFossilGroundwaterAllocationLong_*-12-31.nc  avgNonFossilGroundwaterAllocationLong_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/avgNonFossilGroundwaterAllocationShort_*-12-31.nc avgNonFossilGroundwaterAllocationShort_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/avgTotalGroundwaterAbstraction_*-12-31.nc         avgTotalGroundwaterAbstraction_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/avgTotalGroundwaterAllocationLong_*-12-31.nc      avgTotalGroundwaterAllocationLong_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/avgTotalGroundwaterAllocationShort_*-12-31.nc     avgTotalGroundwaterAllocationShort_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/baseflow_*-12-31.nc                               baseflow_*-12-31.nc
+cdo setunit,"m3"        nc_without_units/channelStorage_*-12-31.nc                         channelStorage_*-12-31.nc
+cdo setunit,"m"         nc_without_units/interceptStor_forest_*-12-31.nc                   interceptStor_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/interceptStor_grassland_*-12-31.nc                interceptStor_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/interceptStor_irrNonPaddy_*-12-31.nc              interceptStor_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/interceptStor_irrPaddy_*-12-31.nc                 interceptStor_irrPaddy_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/interflow_forest_*-12-31.nc                       interflow_forest_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/interflow_grassland_*-12-31.nc                    interflow_grassland_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/interflow_irrNonPaddy_*-12-31.nc                  interflow_irrNonPaddy_*-12-31.nc
+cdo setunit,"m day-1"   nc_without_units/interflow_irrPaddy_*-12-31.nc                     interflow_irrPaddy_*-12-31.nc
+cdo setunit,"m6 day-2"  nc_without_units/m2tDischargeLong_*-12-31.nc                       m2tDischargeLong_*-12-31.nc
+cdo setunit,"m3"        nc_without_units/readAvlChannelStorage_*-12-31.nc                  readAvlChannelStorage_*-12-31.nc
+cdo setunit,"m"         nc_without_units/relativeGroundwaterHead_*-12-31.nc                relativeGroundwaterHead_*-12-31.nc
+cdo setunit,"m3 day-1"  nc_without_units/riverbedExchange_*-12-31.nc                       riverbedExchange_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowCoverSWE_forest_*-12-31.nc                    snowCoverSWE_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowCoverSWE_grassland_*-12-31.nc                 snowCoverSWE_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowCoverSWE_irrNonPaddy_*-12-31.nc               snowCoverSWE_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowCoverSWE_irrPaddy_*-12-31.nc                  snowCoverSWE_irrPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowFreeWater_forest_*-12-31.nc                   snowFreeWater_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowFreeWater_grassland_*-12-31.nc                snowFreeWater_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowFreeWater_irrNonPaddy_*-12-31.nc              snowFreeWater_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/snowFreeWater_irrPaddy_*-12-31.nc                 snowFreeWater_irrPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storGroundwater_*-12-31.nc                        storGroundwater_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storGroundwaterFossil_*-12-31.nc                  storGroundwaterFossil_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storLow_forest_*-12-31.nc                         storLow_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storLow_grassland_*-12-31.nc                      storLow_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storLow_irrNonPaddy_*-12-31.nc                    storLow_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storLow_irrPaddy_*-12-31.nc                       storLow_irrPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storUpp_forest_*-12-31.nc                         storUpp_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storUpp_grassland_*-12-31.nc                      storUpp_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storUpp_irrNonPaddy_*-12-31.nc                    storUpp_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/storUpp_irrPaddy_*-12-31.nc                       storUpp_irrPaddy_*-12-31.nc
+cdo setunit,"m3 s-1"    nc_without_units/subDischarge_*-12-31.nc                           subDischarge_*-12-31.nc
+cdo setunit,"day"       nc_without_units/timestepsToAvgDischarge_*-12-31.nc                timestepsToAvgDischarge_*-12-31.nc
+cdo setunit,"m"         nc_without_units/topWaterLayer_forest_*-12-31.nc                   topWaterLayer_forest_*-12-31.nc
+cdo setunit,"m"         nc_without_units/topWaterLayer_grassland_*-12-31.nc                topWaterLayer_grassland_*-12-31.nc
+cdo setunit,"m"         nc_without_units/topWaterLayer_irrNonPaddy_*-12-31.nc              topWaterLayer_irrNonPaddy_*-12-31.nc
+cdo setunit,"m"         nc_without_units/topWaterLayer_irrPaddy_*-12-31.nc                 topWaterLayer_irrPaddy_*-12-31.nc
+
+ncview *.nc
+
+cd -
+
+set +x
+
