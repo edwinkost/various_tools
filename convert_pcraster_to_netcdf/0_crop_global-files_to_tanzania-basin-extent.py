9f5d8e283cfc683b7ec40c3940c69595942490b2
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-12-11 13:38:24 +0100

    first try

---
+++ convert_pcraster_to_netcdf/0_crop_global-files_to_tanzania-basin-extent.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+
+import pcraster_to_netcdf as pcr2nc
+
+def main():
+    
+    #~ source_path = "/scratch/depfg/sutan101/test_ldd/input/"
+    #~ 
+    #~ target_path = "/scratch/depfg/sutan101/test_ldd/output_2/"
+
+    source_path = "/scratch/depfg/sutan101/data/pcrglobwb2_input_arise/develop/pcrglobwb2_input/"
+    
+    target_path = "/scratch/depfg/sutan101/data/pcrglobwb2_input_arise/develop_tanzania_version_2019_12_11/pcrglobwb2_input/"
+
+    if os.path.exists(target_path): shutil.rmtree(target_path)
+    os.makedirs(target_path)
+    print(target_path)
+    
+    # about os.walk, see https://www.tutorialspoint.com/python/os_walk.htm
+
+    for roots, dirs, files in os.walk(source_path):
+
+        # preparing directories
+        for directory in dirs:
+
+            source_directory = os.path.join(roots, directory)
+            target_directory = source_directory.replace(source_path, target_path)
+            
+            target_directory = target_directory.replace("global", "tanzania")
+
+            if os.path.exists(target_directory): shutil.rmtree(target_directory)
+
+            os.makedirs(target_directory)
+            print(target_directory)
+
+        for file_name in files:
+            
+            print("\n\n")
+
+            # get the full path of source
+            source_file_name = os.path.join(roots, file_name)
+            print(source_file_name)
+            
+            # get target file_name
+            target_file_name = source_file_name.replace(source_path, target_path)
+
+            # make sure that the output directory is ready
+            target_directory = os.path.dirname(source_file_name).replace(source_path, target_path)
+            if os.path.exists(target_directory) == False: os.makedirs(target_directory)
+
+            #~ # - go to the target directory - not necessary
+            #~ os.chdir(target_directory)
+            
+            if target_file_name.endswith(".nc") or target_file_name.endswith(".nc4"):
+
+                # for netcdf files
+
+                # - rename ".nc4" to "nc" (the standard extension of netcdf file is ".nc")
+                if target_file_name.endswith(".nc4"): target_file_name = target_file_name[:-1]
+
+            elif target_file_name.endswith(".map"):  
+
+                # for pcraster map files
+                
+                # shall we also copy them?
+                shutil.copy(source_file_name, target_file_name)
+
+                # convert them to netcdf
+                target_file_name = target_file_name[:-4] + ".nc"
+                
+                msg = "converting " + source_file_name + " to " + target_file_name
+                print(msg)
+                netcdf_zlib_option = True
+                if without_compression: netcdf_zlib_option = False
+                pcr2nc.convert_pcraster_to_netcdf(\
+                                                  input_pcr_map_file = source_file_name,\
+                                                  output_netcdf_file = target_file_name,\
+                                                  variable_name = None,\
+                                                  netcdf_global_attributes = None,\
+                                                  netcdf_y_orientation_from_top_bottom = True,\
+                                                  variable_unit = "unknown",\
+                                                  netcdf_format = "NETCDF4",\
+                                                  netcdf_zlib_option = False,\
+                                                  time_input = None)
+            
+            else:
+                
+                # for other files
+                
+                # just copy
+                msg = "copying " + source_file_name + " to " + target_file_name
+                print(msg)
+                shutil.copy(source_file_name, target_file_name)
+
+            if target_file_name.endswith(".nc") or target_file_name.endswith(".nc4"):
+
+                # cropping
+                cmd = "ncea -d latitude,-16.0,0.0 -d longitude,28.0,41.0 " + (target_file_name)
+                print(cmd_line)
+                os.system(cmd_line)
+
+                cmd = "ncea -d lat,-16.0,0.0 -d lon,28.0,41.0 " + (target_file_name)
+                print(cmd_line)
+                os.system(cmd_line)
+
+    print("\n Done! \n")                          
+                                        
+
+if __name__ == '__main__':
+    sys.exit(main())
