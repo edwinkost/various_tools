250c0632df81e09163738673115d953731422cc7
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2020-03-11 16:17:25 +0100

    first tests eduardo script

--- process_era5-land/download_2000_and_2001.sh
+++ process_era5-land/download_2000_and_2001.sh
@@ -4,20 +4,20 @@ set -x
 # Wind
 
 # -year 2000, months 1 to 12
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Temp 2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Wind 2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Sp   2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Tp   2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Ssr  2000 2000 1 12 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Fa   2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ temp 2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ wind 2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ sp   2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ tp   2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ ssr  2000 2000 1 12 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ fa   2000 2000 1 12 0 0 28 -16 41 &
 
 # -year 2001, month 1 only
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Temp 2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Wind 2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Sp   2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Tp   2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Ssr  2001 2001 1  1 0 0 28 -16 41 &
-python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ Fa   2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ temp 2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ wind 2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ sp   2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ tp   2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ ssr  2001 2001 1  1 0 0 28 -16 41 &
+python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ fa   2001 2001 1  1 0 0 28 -16 41 &
 
 wait
 
