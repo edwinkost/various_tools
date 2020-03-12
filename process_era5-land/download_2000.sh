dba5eb8fcc43c4f4c3d3ab3567a09053d73afc6f
U-DESKTOP-92P3V7F\rikat <rika_small_laptop@mobaxterm>
2020-03-12 01:47:13 +0100

    test process 2000

--- process_era5-land/download_2000.sh
+++ process_era5-land/download_2000.sh
@@ -1,8 +1,6 @@
 
 set -x
 
-# Wind
-
 # -year 2000, months 1 to 12
 python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ temp 2000 2000 1 12 0 0 28 -16 41 &
 python ERA5LandDownload.py /scratch-shared/edwinhs/test_arise_meteo/hourly/ wind 2000 2000 1 12 0 0 28 -16 41 &
