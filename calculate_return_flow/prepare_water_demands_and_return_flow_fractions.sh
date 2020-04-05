15815f67120fdf7cdc13dde5a90e496cc7b3b7e9
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-05 23:07:38 +0200

    start processing files for Edward Jones

--- calculate_return_flow/prepare_water_demands_and_return_flow_fractions.sh
+++ calculate_return_flow/prepare_water_demands_and_return_flow_fractions.sh
@@ -13,36 +13,36 @@ MAIN_SOURCE_DIR="/projects/0/dfguu/data/hydroworld/pcrglobwb2_input_release/vers
 # for domestic water demands
 
 OUTPUT_DIR="/scratch-shared/edwinhs/data_for_edward_jones/water_demands_and_return_flow_fractions/domestic/"
-mkdir -p ${MAIN_OUTPUT_DIR}
-cd ${MAIN_OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+rm *.nc
 
 SOURCE_FILE=${MAIN_SOURCE_DIR}/domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc
-cdo -L -setyear,2011 -selyear,2010 domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc domestic_demand_2011.nc
-cdo -L -setyear,2012 -selyear,2010 domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc domestic_demand_2012.nc
-cdo -L -setyear,2013 -selyear,2010 domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc domestic_demand_2013.nc
-cdo -L -setyear,2014 -selyear,2010 domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc domestic_demand_2014.nc
-cdo -L -setyear,2015 -selyear,2010 domestic/complete_uncompressed/domestic_water_demand_version_april_2015.nc domestic_demand_2015.nc
+cdo -L -setyear,2011 -selyear,2010 ${SOURCE_FILE} domestic_demand_2011.nc
+cdo -L -setyear,2012 -selyear,2010 ${SOURCE_FILE} domestic_demand_2012.nc
+cdo -L -setyear,2013 -selyear,2010 ${SOURCE_FILE} domestic_demand_2013.nc
+cdo -L -setyear,2014 -selyear,2010 ${SOURCE_FILE} domestic_demand_2014.nc
+cdo -L -setyear,2015 -selyear,2010 ${SOURCE_FILE} domestic_demand_2015.nc
 cdo -L -mergetime ${SOURCE_FILE} domestic_*.nc domestic_water_demand_gmd_paper.nc
 
-# - calculate return flow fractions
 cdo -L expr,"dom_ret_flow_frac = ( (domesticGrossDemand > 0.0)) ? 1-(domesticNettoDemand/domesticGrossDemand) : 0.0)" domestic_water_demand_gmd_paper.nc domestic_return_flow_fraction_gmd_paper.nc 
 
 
 # for industry water demands
 
 OUTPUT_DIR="/scratch-shared/edwinhs/data_for_edward_jones/water_demands_and_return_flow_fractions/industry/"
-mkdir -p ${MAIN_OUTPUT_DIR}
-cd ${MAIN_OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
+cd ${OUTPUT_DIR}
+rm *.nc
 
 SOURCE_FILE=${MAIN_SOURCE_DIR}/industry/complete_uncompressed/industry_water_demand_version_april_2015.nc
-cdo -L -setyear,2011 -selyear,2010 industry/complete_uncompressed/industry_water_demand_version_april_2015.nc industry_demand_2011.nc
-cdo -L -setyear,2012 -selyear,2010 industry/complete_uncompressed/industry_water_demand_version_april_2015.nc industry_demand_2012.nc
-cdo -L -setyear,2013 -selyear,2010 industry/complete_uncompressed/industry_water_demand_version_april_2015.nc industry_demand_2013.nc
-cdo -L -setyear,2014 -selyear,2010 industry/complete_uncompressed/industry_water_demand_version_april_2015.nc industry_demand_2014.nc
-cdo -L -setyear,2015 -selyear,2010 industry/complete_uncompressed/industry_water_demand_version_april_2015.nc industry_demand_2015.nc
+cdo -L -setyear,2011 -selyear,2010 ${SOURCE_FILE} industry_demand_2011.nc
+cdo -L -setyear,2012 -selyear,2010 ${SOURCE_FILE} industry_demand_2012.nc
+cdo -L -setyear,2013 -selyear,2010 ${SOURCE_FILE} industry_demand_2013.nc
+cdo -L -setyear,2014 -selyear,2010 ${SOURCE_FILE} industry_demand_2014.nc
+cdo -L -setyear,2015 -selyear,2010 ${SOURCE_FILE} industry_demand_2015.nc
 cdo -L -mergetime ${SOURCE_FILE} industry_*.nc industry_water_demand_gmd_paper.nc
 
-# - calculate return flow fractions
 cdo -L expr,"in_ret_flow_frac = ( (industryGrossDemand > 0.0)) ? 1-(industryNettoDemand/industryGrossDemand) : 0.0)" industry_water_demand_gmd_paper.nc industry_return_flow_fraction_gmd_paper.nc 
 
 
