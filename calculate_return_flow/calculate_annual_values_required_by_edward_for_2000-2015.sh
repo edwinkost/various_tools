0d2649be95c57266c0c8582fa90b4a295c0508a8
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-08 23:30:01 +0200

    calculate annual values for Edward - 5min

---
+++ calculate_return_flow/calculate_annual_values_required_by_edward_for_2000-2015.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bash calculate_annual_values_required_by_edward.sh 2000
+bash calculate_annual_values_required_by_edward.sh 2001
+bash calculate_annual_values_required_by_edward.sh 2002
+bash calculate_annual_values_required_by_edward.sh 2003
+bash calculate_annual_values_required_by_edward.sh 2004
+bash calculate_annual_values_required_by_edward.sh 2005
+bash calculate_annual_values_required_by_edward.sh 2006
+bash calculate_annual_values_required_by_edward.sh 2007
+bash calculate_annual_values_required_by_edward.sh 2008
+bash calculate_annual_values_required_by_edward.sh 2009
+bash calculate_annual_values_required_by_edward.sh 2010
+bash calculate_annual_values_required_by_edward.sh 2011
+bash calculate_annual_values_required_by_edward.sh 2012
+bash calculate_annual_values_required_by_edward.sh 2013
+bash calculate_annual_values_required_by_edward.sh 2014
+bash calculate_annual_values_required_by_edward.sh 2015
+
