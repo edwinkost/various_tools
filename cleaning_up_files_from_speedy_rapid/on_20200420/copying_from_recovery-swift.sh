99c0760cba38717136260f2ba35ef9772af7604e
Edwin Sutanudjaja <sutan101@speedy.geo.uu.nl>
2020-04-20 13:14:34 +0200

    cleaning up recovery-swift /home/edwin/

---
+++ cleaning_up_files_from_speedy_rapid/on_20200420/copying_from_recovery-swift.sh
@@ -0,0 +1,22 @@
+set -x
+
+SOURCE="sutan101@speedy.geo.uu.nl:/nfsarchive/recovery-swift/home/edwin/*"
+
+TARGET="/scratch/depfg/sutan101/files_from_speedy_rapid_etc_until_202004020/recovery-swift/home_edwin/"
+
+# the following options should also include copying real files and folders of symlinks
+OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress --copy-links"
+
+# the following options should make symlinks broken
+#~ OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress"
+
+# rsync command
+rsync ${OPTIONS} ${SOURCE} ${TARGET}
+
+# rsync command - for final checking
+rsync ${OPTIONS} ${SOURCE} ${TARGET} 
+
+# go back to the original folder (where this script is excecuted)
+cd -
+
+set +x
