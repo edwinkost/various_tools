490d4e0059d6df75eaa565bab64e897a6d13935b
Edwin Sutanudjaja <sutan101@speedy.geo.uu.nl>
2020-04-20 13:16:44 +0200

    cleaning up recovery-swift /home/edwin/

--- cleaning_up_files_from_speedy_rapid/on_20200420/copying_from_recovery-swift.sh
+++ cleaning_up_files_from_speedy_rapid/on_20200420/copying_from_recovery-swift.sh
@@ -3,6 +3,7 @@ set -x
 SOURCE="sutan101@speedy.geo.uu.nl:/nfsarchive/recovery-swift/home/edwin/*"
 
 TARGET="/scratch/depfg/sutan101/files_from_speedy_rapid_etc_until_202004020/recovery-swift/home_edwin/"
+mkdir -p ${TARGET}
 
 # the following options should also include copying real files and folders of symlinks
 OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress --copy-links"
