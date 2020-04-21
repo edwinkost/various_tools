af17d9dc4465639425b866f5f4f17eeaca1d029b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-04-21 22:19:05 +0200

    start copying files from nfsarchive

--- cleaning_up_files_from_speedy_rapid/on_20200420/copying_from_nfsarchive.sh
+++ cleaning_up_files_from_speedy_rapid/on_20200420/copying_from_nfsarchive.sh
@@ -1,9 +1,8 @@
 set -x
 
-SOURCE="sutan101@rapid.geo.uu.nl:/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/*"
+SOURCE="/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/"
 
-TARGET="/scratch/depfg/sutan101/files_from_speedy_rapid_etc_until_202004020/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/"
-mkdir -p ${TARGET}
+TARGET="sutan101@eejit.geo.uu.nl:/scratch/depfg/sutan101/files_from_speedy_rapid_etc_until_202004020/nfsarchive/edwin-emergency-backup-DO-NOT-DELETE/"
 
 # the following options should also include copying real files and folders of symlinks
 OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress --copy-links"
@@ -11,11 +10,14 @@ OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progr
 # the following options should make symlinks broken
 #~ OPTIONS="--recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress"
 
+# go to the SOURCE FOLDER
+cd ${SOURCE}
+
 # rsync command
-rsync ${OPTIONS} ${SOURCE} ${TARGET}
+rsync ${OPTIONS} * ${TARGET}
 
 # rsync command - for final checking
-rsync ${OPTIONS} ${SOURCE} ${TARGET} 
+rsync ${OPTIONS} * ${TARGET} 
 
 # go back to the original folder (where this script is excecuted)
 cd -
