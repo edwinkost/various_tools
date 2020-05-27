12f6633b8064168caabf2530d3209302c500990d
U-SOLISCOM\Sutan101 <Sutan101@UU078667.soliscom.uu.nl>
2020-05-27 11:27:11 +0200

    in progress for sellonlatbox

--- cdo_sellonlatbox/maritsa_paul_greenpeace/cdo_sellonlatbox.py
+++ cdo_sellonlatbox/maritsa_paul_greenpeace/cdo_sellonlatbox.py
@@ -4,8 +4,7 @@ import os
 import sys
 
 # example of command line:
-CDO_TIMESTAT_DATE='last' cdo -L -monavg -sellonlatbox,0,2,0,2 -mergetime /scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_irrigation_water_use/historical/1951-2005/gfdl-esm2m/irrigationWaterWithdrawal_annuaTot_output_*.nc test.nc
-
+CDO_TIMESTAT_DATE='last' cdo -L -settime,00:00:00  -monavg -sellonlatbox,100,120,-10,10 -mergetime /scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_irrigation_water_use/historical/1951-2005/gfdl-esm2m/irrigationWaterWithdrawal_annuaTot_output_*.nc test.nc
 lonlatbox   = ""
 
 input_files = 
