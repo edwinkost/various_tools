e84e6975385169031b470652597c2caf8f6b56bb
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 01:43:23 +0200

    test python script

--- cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox.sh
+++ cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox.sh
@@ -7,12 +7,12 @@ set -x
 LONLATBOX="22.0,30.0,40.0,45.0"
 
 # - discharge
-INPUT_FILES="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/historical/1951-2005/gfdl-esm2m/discharge_monthAvg_output"
+INPUT_FILES=/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/historical/1951-2005/gfdl-esm2m/discharge_monthAvg_output
 OUTPUT_FILE=/scratch-shared/edwinhs/pcr-globwb-aqueduct/maritsa/test/historical/1951-2005/gfdl-esm2m/discharge_monthAvg_output_1951-2005.nc 
 python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - temperature
-INPUT_FILES="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/historical/1951-2005/gfdl-esm2m/temperature_annuaAvg_output"
+INPUT_FILES=/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/historical/1951-2005/gfdl-esm2m/temperature_annuaAvg_output
 OUTPUT_FILE=/scratch-shared/edwinhs/pcr-globwb-aqueduct/maritsa/test/historical/1951-2005/gfdl-esm2m/temperature_monthAvg_output_1951-2005.nc 
 python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
