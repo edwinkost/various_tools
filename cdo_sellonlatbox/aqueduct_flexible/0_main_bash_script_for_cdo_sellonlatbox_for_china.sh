f6a48d1ee86e47e43f48d36dd5884bc581fd6c20
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 10:47:47 +0200

    get data for china

--- cdo_sellonlatbox/aqueduct_flexible/0_main_bash_script_for_cdo_sellonlatbox_for_china.sh
+++ cdo_sellonlatbox/aqueduct_flexible/0_main_bash_script_for_cdo_sellonlatbox_for_china.sh
@@ -3,9 +3,9 @@ set -x
 
 MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/china/"
 
-LONLATBOX="72.0,136.0,53.0,74.0"
+#~ Paul: the bounding box for China is 54°N 73°E-18°N 135.5°E
 
-#~ the bounding box for China is 54°N 73°E-18°N 135.5°E
+LONLATBOX="72.0,137.0,16.0,56.0"
 
 bash bash_script_for_cdo_sellonlatbox_historical.sh historical ${MAIN_OUT_FOLDER} ${LONLATBOX}
 
