658fa04fa1826417d2d9a72f6baf06b2b1ed278b
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 10:53:33 +0200

    get data for china

--- cdo_sellonlatbox/aqueduct_flexible/0_main_bash_script_for_cdo_sellonlatbox_for_china.sh
+++ cdo_sellonlatbox/aqueduct_flexible/0_main_bash_script_for_cdo_sellonlatbox_for_china.sh
@@ -9,9 +9,12 @@ LONLATBOX="72.0,137.0,16.0,56.0"
 
 bash bash_script_for_cdo_sellonlatbox_historical.sh historical ${MAIN_OUT_FOLDER} ${LONLATBOX}
 
-bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp2p6     ${MAIN_OUT_FOLDER} ${LONLATBOX}
-bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp4p5     ${MAIN_OUT_FOLDER} ${LONLATBOX}
-bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp6p0     ${MAIN_OUT_FOLDER} ${LONLATBOX}
-bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp8p5     ${MAIN_OUT_FOLDER} ${LONLATBOX}
+bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp2p6     ${MAIN_OUT_FOLDER} ${LONLATBOX} &
+bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp6p0     ${MAIN_OUT_FOLDER} ${LONLATBOX} &
+wait
+
+bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp4p5     ${MAIN_OUT_FOLDER} ${LONLATBOX} &
+bash bash_script_for_cdo_sellonlatbox_rcpxxx.sh     rcp8p5     ${MAIN_OUT_FOLDER} ${LONLATBOX} &
+wait
 
 set +x
