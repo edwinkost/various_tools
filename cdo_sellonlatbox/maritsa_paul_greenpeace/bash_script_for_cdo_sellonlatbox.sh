c3e676fae0184d761699c25c4afe6fe6e2244ea8
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 02:45:00 +0200

    test python script

--- cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox.sh
+++ cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox.sh
@@ -5,7 +5,6 @@
 set -x
 
 MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/"
-MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/maritsa/test/"
 
 LONLATBOX="22.0,30.0,40.0,45.0"
 
@@ -15,34 +14,50 @@ PERIODYR="1951-2005"
 
 GCM_CODE="gfdl-esm2m"
 
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/maritsa/test/"
 
 
 # - discharge
-FILE_VARIABLE_NAME=discharge_monthAvg_output
+FILE_VARIABLE_NAME=discharge_monthAvg
 INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
 OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
 python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - temperature - annual resolution
-FILE_VARIABLE_NAME=temperature_annuaAvg_output
+FILE_VARIABLE_NAME=temperature_annuaAvg
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - precipitation
 FILE_VARIABLE_NAME=precipitation_monthTot
-
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - domestic withdrawal
 FILE_VARIABLE_NAME=domesticWaterWithdrawal_monthTot
-
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - industrial withdrawal
-FILE_VARIABLE_NAME=industryWaterWithdrawal_monthTot_output
+FILE_VARIABLE_NAME=industryWaterWithdrawal_monthTot
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 # - livestock withdrawal
-FILE_VARIABLE_NAME=industryWaterWithdrawal_monthTot_output
+FILE_VARIABLE_NAME=livestockWaterWithdrawal_monthTot
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
-# - irr
-irrGrossDemand_monthTot_outpu
-
+# - irrigation withdrawal
+FILE_VARIABLE_NAME=irrGrossDemand_monthTot_outpu
+INPUT_FILES=${MAIN_INP_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}
+OUTPUT_FILE=${MAIN_OUT_FOLDER}/${SCENARIO}/${PERIODYR}/${GCM_CODE}/${FILE_VARIABLE_NAME}_output_${PERIODYR}_${SCENARIO}_${GCM_CODE}.nc 
+python cdo_sellonlatbox.py ${LONLATBOX} ${INPUT_FILES} ${OUTPUT_FILE} &
 
 wait
 
