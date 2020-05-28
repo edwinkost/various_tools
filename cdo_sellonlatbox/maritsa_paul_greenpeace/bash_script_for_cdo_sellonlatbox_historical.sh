452049ffcd9c4c06648a1b30783a7d9ddb1c97e1
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 03:19:04 +0200

    maritsa - historical

---
+++ cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox_historical.sh
@@ -0,0 +1,23 @@
+
+set -x
+
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/maritsa/"
+
+LONLATBOX="22.0,30.0,40.0,45.0"
+
+MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_all/"
+MAIN_INP_FOLDER_LIVESTOCK="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_livestock/"
+
+
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1951-2005 gfdl-esm2m     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1951-2005 hadgem2-es     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1951-2005 ipsl-cm5a-lr   ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1951-2005 miroc-esm-chem ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1951-2005 noresm1-m      ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} historical 1958-2001 watch          ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+
+
+wait
+
+set +x
