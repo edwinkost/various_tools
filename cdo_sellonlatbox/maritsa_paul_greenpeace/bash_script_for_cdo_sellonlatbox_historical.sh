d7a4caf8317fdbbeeb82a869a7084cc115908533
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2020-05-28 03:28:10 +0200

    maritsa - historical

--- cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox_historical.sh
+++ cdo_sellonlatbox/maritsa_paul_greenpeace/bash_script_for_cdo_sellonlatbox_historical.sh
@@ -11,11 +11,11 @@ MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_water_use_
 MAIN_INP_FOLDER_LIVESTOCK="/scratch-shared/edwinhs/pcr-globwb-aqueduct/aqueduct_livestock/"
 
 
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 gfdl-esm2m     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 hadgem2-es     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 ipsl-cm5a-lr   ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 miroc-esm-chem ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 noresm1-m      ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
-bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1958-2001 watch          ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK}
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 gfdl-esm2m     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 hadgem2-es     ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 ipsl-cm5a-lr   ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 miroc-esm-chem ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1951-2005 noresm1-m      ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
+bash bash_script_for_cdo_sellonlatbox.sh ${MAIN_OUT_FOLDER} ${LONLATBOX} ${SCENARIO} 1958-2001 watch          ${MAIN_INP_FOLDER} ${MAIN_INP_FOLDER_LIVESTOCK} &
 
 set +x
