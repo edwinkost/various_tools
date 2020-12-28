a22fd9861133143dcc613c1d74105b1af520a323
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-12-28 22:48:53 +0100

    merging nc files from aqueduct flood runs

--- cdo_merging/aqueduct_flood_runs/cdo_mergetime.py
+++ cdo_merging/aqueduct_flood_runs/cdo_mergetime.py
@@ -14,36 +14,35 @@ variables = [
 "discharge_monthAvg",
 "precipitation_monthTot",
 
-# ~ "baseflow_monthTot",
-# ~ "gwRecharge_monthTot",
+"baseflow_monthTot",
+"gwRecharge_monthTot",
 
-# ~ "irrGrossDemand_monthTot",
-# ~ "nonIrrGrossDemand_monthTot",
+"irrGrossDemand_monthTot",
+"nonIrrGrossDemand_monthTot",
 
-# ~ "domesticWaterWithdrawal_monthTot",
-# ~ "industryWaterWithdrawal_monthTot",
-# ~ "livestockWaterWithdrawal_monthTot",
-# ~ "irrNonPaddyWaterWithdrawal_monthTot",
-# ~ "irrPaddyWaterWithdrawal_monthTot",
+"domesticWaterWithdrawal_monthTot",
+"industryWaterWithdrawal_monthTot",
+"livestockWaterWithdrawal_monthTot",
+"irrNonPaddyWaterWithdrawal_monthTot",
+"irrPaddyWaterWithdrawal_monthTot",
 
-# ~ "channelStorage_monthAvg",
+"channelStorage_monthAvg",
 
-# ~ "desalinationAbstraction_monthTot",
-# ~ "surfaceWaterAbstraction_monthTot",
-# ~ "totalGroundwaterAbstraction_monthTot",
-# ~ "fossilGroundwaterAbstraction_monthTot",
+"desalinationAbstraction_monthTot",
+"surfaceWaterAbstraction_monthTot",
+"totalGroundwaterAbstraction_monthTot",
+"fossilGroundwaterAbstraction_monthTot",
 
-# ~ "storUppTotal_monthAvg",
-# ~ "storLowTotal_monthAvg",
-# ~ "storGroundwater_monthAvg",
+"storUppTotal_monthAvg",
+"storLowTotal_monthAvg",
+"storGroundwater_monthAvg",
 
-# ~ "totalWaterStorageThickness_monthAvg",
+"totalWaterStorageThickness_monthAvg",
 
-# ~ "totalRunoff_monthTot",
-# ~ "runoff_monthTot",
+"totalRunoff_monthTot",
+"runoff_monthTot",
 
-# ~ "totalEvaporation_monthTot",
-
+"totalEvaporation_monthTot",
 
 ]
 
