2cf1fccffb634c64530e8e125bc67df622240dea
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2020-12-28 22:05:21 +0100

    mergetime for nc files of aqueduct runs

--- cdo_merging/cdo_mergetime_aqueduct_flood.py
+++ cdo_merging/cdo_mergetime_aqueduct_flood.py
@@ -11,39 +11,39 @@ years = sys.argv[3]
 
 variables = [
 
-"baseflow_monthTot",
-"gwRecharge_monthTot",
-
-"irrGrossDemand_monthTot",
-"nonIrrGrossDemand_monthTot",
-
-"domesticWaterWithdrawal_monthTot",
-"industryWaterWithdrawal_monthTot",
-"livestockWaterWithdrawal_monthTot",
-"irrNonPaddyWaterWithdrawal_monthTot",
-"irrPaddyWaterWithdrawal_monthTot",
-
-"channelStorage_monthAvg",
-
-"desalinationAbstraction_monthTot",
-"surfaceWaterAbstraction_monthTot",
-"totalGroundwaterAbstraction_monthTot",
-"fossilGroundwaterAbstraction_monthTot",
-
-"storUppTotal_monthAvg",
-"storLowTotal_monthAvg",
-"storGroundwater_monthAvg",
-
-"totalWaterStorageThickness_monthAvg",
-
 "discharge_monthAvg",
-
 "precipitation_monthTot",
 
-"totalEvaporation_monthTot",
+# ~ "baseflow_monthTot",
+# ~ "gwRecharge_monthTot",
 
-"totalRunoff_monthTot",
-"runoff_monthTot"
+# ~ "irrGrossDemand_monthTot",
+# ~ "nonIrrGrossDemand_monthTot",
+
+# ~ "domesticWaterWithdrawal_monthTot",
+# ~ "industryWaterWithdrawal_monthTot",
+# ~ "livestockWaterWithdrawal_monthTot",
+# ~ "irrNonPaddyWaterWithdrawal_monthTot",
+# ~ "irrPaddyWaterWithdrawal_monthTot",
+
+# ~ "channelStorage_monthAvg",
+
+# ~ "desalinationAbstraction_monthTot",
+# ~ "surfaceWaterAbstraction_monthTot",
+# ~ "totalGroundwaterAbstraction_monthTot",
+# ~ "fossilGroundwaterAbstraction_monthTot",
+
+# ~ "storUppTotal_monthAvg",
+# ~ "storLowTotal_monthAvg",
+# ~ "storGroundwater_monthAvg",
+
+# ~ "totalWaterStorageThickness_monthAvg",
+
+# ~ "totalRunoff_monthTot",
+# ~ "runoff_monthTot",
+
+# ~ "totalEvaporation_monthTot",
+
 
 ]
 
