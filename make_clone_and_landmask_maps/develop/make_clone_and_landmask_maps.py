25a5c3668e34bded73fa28f574dab79efa2d69e4
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-15 22:44:21 +0100

    in progress making clone maps

--- make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
+++ make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
@@ -314,8 +314,8 @@ def main():
                     mask_selected_nominal = pcr.ifthen(mask_selected_boolean_from_clump, pcr.nominal(assigned_number))
                     subdomains_final = pcr.cover(subdomains_final, mask_selected_nominal)
                     
-    # kill all aguila processes if exist
-    os.system('killall aguila')
+    # ~ # kill all aguila processes if exist
+    # ~ os.system('killall aguila')
     
     pcr.aguila(subdomains_final)
 
