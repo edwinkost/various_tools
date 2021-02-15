191d7e4c366e23c8093e0dd4295ef3da7908e7df
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-15 20:20:42 +0100

    in progress making clone maps

--- make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
+++ make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
@@ -136,7 +136,7 @@ def main():
     island_map = pcr.areamajority(pcr.nominal(island_map_rep_ids), island_map)
     
     # identify the biggest island for every group of small islands within the windows 10x10 arcdeg cells
-    large_island_map  = pcr.ifthen(pcr.scalar(island_map) == pcr.windowmaximum(pcr.scalar(island_map), 10.), island_map)
+    large_island_map  = pcr.ifthen(pcr.scalar(island_map) == pcr.windowminimum(pcr.scalar(island_map), 10.), island_map)
     pcr.aguila(large_island_map)
     
 
