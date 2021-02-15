49c5ac58d9e3f0268f36d00027a4d70b5b5f608c
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-15 20:47:20 +0100

    in progress making clone maps

--- make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
+++ make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps.py
@@ -142,8 +142,8 @@ def main():
     # -- map of smaller islands, sorted from the largest one
     island_map = pcr.areamajority(pcr.nominal(island_map_rep_ids), island_map)
     
-    # identify the biggest island for every group of small islands within the windows 10x10 arcdeg cells
-    large_island_map  = pcr.ifthen(pcr.scalar(island_map) == pcr.windowminimum(pcr.scalar(island_map), 10.), island_map)
+    # identify the biggest island for every group of small islands within the windows 20x20 arcdeg cells
+    large_island_map  = pcr.ifthen(pcr.scalar(island_map) == pcr.windowminimum(pcr.scalar(island_map), 20.), island_map)
     pcr.aguila(large_island_map)
     
 
@@ -157,8 +157,8 @@ def main():
 
 
     # merge biggest islands and big catchments
-    # ~ large_catchment_and_island_map = pcr.cover(large_catchment_map, large_island_map)
-    large_catchment_and_island_map = pcr.cover(large_island_map, large_catchment_map)
+    large_catchment_and_island_map = pcr.cover(large_catchment_map, large_island_map)
+    # ~ large_catchment_and_island_map = pcr.cover(large_island_map, large_catchment_map)
     large_catchment_and_island_map_size = pcr.areatotal(pcr.spatial(pcr.scalar(1.0)), large_catchment_and_island_map)
 
     
