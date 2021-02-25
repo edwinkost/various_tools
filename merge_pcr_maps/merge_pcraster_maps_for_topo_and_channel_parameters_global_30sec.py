ca74f639115eaf199e37a39d5667ab60db4df0ed
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-02-25 07:29:55 +0100

    merge_pcraster_maps_for_topo_and_channel_parameters_global_30sec.py

--- merge_pcr_maps/merge_pcraster_maps_for_topo_and_channel_parameters_global_30sec.py
+++ merge_pcr_maps/merge_pcraster_maps_for_topo_and_channel_parameters_global_30sec.py
@@ -336,6 +336,7 @@ inputDir = os.path.join(inputDirRoot,areas[0], 'map')
 # - without date
 files = getFileList(inputDir, '.map')
 
+print(inputDir)
 print(files)
 
 # get output file names and each of their corresponding input files
