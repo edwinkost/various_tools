54b4aaa9e70413c8c41cb111bda0e874bf290649
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-01 16:31:32 +0100

    convert pcraster to netcdf - gw parameters 30sec

--- convert_pcraster_to_netcdf/bash_convert_gw_parameters_30sec.sh
+++ convert_pcraster_to_netcdf/bash_convert_gw_parameters_30sec.sh
@@ -12,9 +12,9 @@ INP_DIR="/scratch/depfg/sutan101/recession_coefficient_version_2021-02-XX/30sec/
 #~ -r--r--r-- 1 sutan101 depfg 3.5G Mar  1 11:46 ../k_conductivity_aquifer_filled.map
 #~ -r--r--r-- 1 sutan101 depfg 3.5G Mar  1 12:17 ../specific_yield_aquifer_filled.map
 
-python pcraster_to_netcdf.py ${INP_DIR} -inp recession_coefficient_30sec.map   -out recession_coefficient_30sec.nc         -unt "day-1"
-python pcraster_to_netcdf.py ${INP_DIR} -inp k_conductivity_aquifer_filled.map -out k_conductivity_aquifer_filled_30sec.nc -unt "m.day-1"
-python pcraster_to_netcdf.py ${INP_DIR} -inp specific_yield_aquifer_filled.map -out specific_yield_aquifer_filled_30sec.nc -unt "m3.m-3"
+python pcraster_to_netcdf.py -inp ${INP_DIR}/recession_coefficient_30sec.map   -out ${OUT_DIR}/recession_coefficient_30sec.nc         -unt "day-1"
+python pcraster_to_netcdf.py -inp ${INP_DIR}/k_conductivity_aquifer_filled.map -out ${OUT_DIR}/k_conductivity_aquifer_filled_30sec.nc -unt "m.day-1"
+python pcraster_to_netcdf.py -inp ${INP_DIR}/specific_yield_aquifer_filled.map -out ${OUT_DIR}/specific_yield_aquifer_filled_30sec.nc -unt "m3.m-3"
 
 cd ${OUT_DIR}
 ncview *.nc
