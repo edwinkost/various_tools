28f94e5602b5c825385739ae9d647d4374f971ad
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-02 10:44:25 +0100

    calculate direct runoff from urban

---
+++ calculate_irr_return_flow/calculate_monthly_direct_runoff_contribution_for_all_years.sh
@@ -0,0 +1,58 @@
+bash calculate_monthly_direct_runoff_contribution.sh 1980 1980 &
+bash calculate_monthly_direct_runoff_contribution.sh 1981 1981 &
+bash calculate_monthly_direct_runoff_contribution.sh 1982 1982 &
+bash calculate_monthly_direct_runoff_contribution.sh 1983 1983 &
+bash calculate_monthly_direct_runoff_contribution.sh 1984 1984 &
+bash calculate_monthly_direct_runoff_contribution.sh 1985 1985 &
+bash calculate_monthly_direct_runoff_contribution.sh 1986 1986 &
+bash calculate_monthly_direct_runoff_contribution.sh 1987 1987 &
+bash calculate_monthly_direct_runoff_contribution.sh 1988 1988 &
+bash calculate_monthly_direct_runoff_contribution.sh 1989 1989 &
+bash calculate_monthly_direct_runoff_contribution.sh 1990 1990 &
+bash calculate_monthly_direct_runoff_contribution.sh 1991 1991 &
+bash calculate_monthly_direct_runoff_contribution.sh 1992 1992 &
+bash calculate_monthly_direct_runoff_contribution.sh 1993 1993 &
+bash calculate_monthly_direct_runoff_contribution.sh 1994 1994 &
+bash calculate_monthly_direct_runoff_contribution.sh 1995 1995 &
+bash calculate_monthly_direct_runoff_contribution.sh 1996 1996 &
+bash calculate_monthly_direct_runoff_contribution.sh 1997 1997 &
+bash calculate_monthly_direct_runoff_contribution.sh 1998 1998 &
+bash calculate_monthly_direct_runoff_contribution.sh 1999 1999 &
+bash calculate_monthly_direct_runoff_contribution.sh 2000 2000 &
+bash calculate_monthly_direct_runoff_contribution.sh 2001 2001 &
+bash calculate_monthly_direct_runoff_contribution.sh 2002 2002 &
+bash calculate_monthly_direct_runoff_contribution.sh 2003 2003 &
+bash calculate_monthly_direct_runoff_contribution.sh 2004 2004 &
+bash calculate_monthly_direct_runoff_contribution.sh 2005 2005 &
+bash calculate_monthly_direct_runoff_contribution.sh 2006 2006 &
+bash calculate_monthly_direct_runoff_contribution.sh 2007 2007 &
+bash calculate_monthly_direct_runoff_contribution.sh 2008 2008 &
+bash calculate_monthly_direct_runoff_contribution.sh 2009 2009 &
+bash calculate_monthly_direct_runoff_contribution.sh 2010 2010 &
+bash calculate_monthly_direct_runoff_contribution.sh 2011 2010 &
+bash calculate_monthly_direct_runoff_contribution.sh 2012 2010 &
+bash calculate_monthly_direct_runoff_contribution.sh 2013 2010 &
+bash calculate_monthly_direct_runoff_contribution.sh 2014 2010 &
+bash calculate_monthly_direct_runoff_contribution.sh 2015 2010 &
+wait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
