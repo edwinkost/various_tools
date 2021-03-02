d6badfcfb63e7eb423a98d8be4515865ae02b496
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-02 10:11:32 +0100

    calculate monthly irrigation and return flow

---
+++ calculate_irr_return_flow/calculate_monthly_irr_return_flow_for_all_years.sh
@@ -0,0 +1,58 @@
+bash calculate_monthly_irr_return_flow.sh 1980 &
+bash calculate_monthly_irr_return_flow.sh 1981 &
+bash calculate_monthly_irr_return_flow.sh 1982 &
+bash calculate_monthly_irr_return_flow.sh 1983 &
+bash calculate_monthly_irr_return_flow.sh 1984 &
+bash calculate_monthly_irr_return_flow.sh 1985 &
+bash calculate_monthly_irr_return_flow.sh 1986 &
+bash calculate_monthly_irr_return_flow.sh 1987 &
+bash calculate_monthly_irr_return_flow.sh 1988 &
+bash calculate_monthly_irr_return_flow.sh 1989 &
+bash calculate_monthly_irr_return_flow.sh 1990 &
+bash calculate_monthly_irr_return_flow.sh 1991 &
+bash calculate_monthly_irr_return_flow.sh 1992 &
+bash calculate_monthly_irr_return_flow.sh 1993 &
+bash calculate_monthly_irr_return_flow.sh 1994 &
+bash calculate_monthly_irr_return_flow.sh 1995 &
+bash calculate_monthly_irr_return_flow.sh 1996 &
+bash calculate_monthly_irr_return_flow.sh 1997 &
+bash calculate_monthly_irr_return_flow.sh 1998 &
+bash calculate_monthly_irr_return_flow.sh 1999 &
+bash calculate_monthly_irr_return_flow.sh 2000 &
+bash calculate_monthly_irr_return_flow.sh 2001 &
+bash calculate_monthly_irr_return_flow.sh 2002 &
+bash calculate_monthly_irr_return_flow.sh 2003 &
+bash calculate_monthly_irr_return_flow.sh 2004 &
+bash calculate_monthly_irr_return_flow.sh 2005 &
+bash calculate_monthly_irr_return_flow.sh 2006 &
+bash calculate_monthly_irr_return_flow.sh 2007 &
+bash calculate_monthly_irr_return_flow.sh 2008 &
+bash calculate_monthly_irr_return_flow.sh 2009 &
+bash calculate_monthly_irr_return_flow.sh 2010 &
+bash calculate_monthly_irr_return_flow.sh 2011 &
+bash calculate_monthly_irr_return_flow.sh 2012 &
+bash calculate_monthly_irr_return_flow.sh 2013 &
+bash calculate_monthly_irr_return_flow.sh 2014 &
+bash calculate_monthly_irr_return_flow.sh 2015 &
+wait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
