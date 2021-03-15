f324423345fde09d03b85d5762b17ac7ed46e1fe
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-03-15 11:34:28 +0100

    make clone and landmask for africa

--- make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps_30sec_for_africa_only.py
+++ make_clone_and_landmask_maps/develop/make_clone_and_landmask_maps_30sec_for_africa_only.py
@@ -71,10 +71,10 @@ global_clone_map_file = "/scratch/depfg/sutan101/data/pcrglobwb_input_arise/deve
 global_ldd_inp_file = "/scratch/depfg/sutan101/data/global_ldd_reservoirs_and_lakes_before_data_lost_on_eejit/lddsound_30sec_version_202005XX.map"
 
 # global subdomain file
-global_subdomain_file = "/scratch/depfg/sutan101/make_global_subdomains/version_2021-02-17/general_subdomains_using_threshold_of_25_cells/global_subdomains_30min_final_filled.map"
+global_subdomain_file = "/scratch/depfg/sutan101/make_global_subdomains/version_2021-02-17/general_subdomains_using_threshold_of_50_cells/global_subdomains_30min_final_filled.map"
 
 # output_folder
-out_folder            = "/scratch/depfg/sutan101/make_global_subdomains/version_2021-03-15/africa/30sec/"
+out_folder            = "/scratch/depfg/sutan101/make_global_subdomains/version_2021-03-15/africa/30sec_50_cells/"
 
 # cell size in arcmin
 # - 0.5 arcmin = 30 arcsec
