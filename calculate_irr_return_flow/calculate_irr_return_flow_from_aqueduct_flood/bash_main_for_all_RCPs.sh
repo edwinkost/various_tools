08b9427fcb9227112727df744d35fdf09dcb5734
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-19 14:20:24 +0200

    calculate irr return flow from aqueduct flood for edward

--- calculate_irr_return_flow/calculate_irr_return_flow_from_aqueduct_flood/bash_main_for_all_RCPs.sh
+++ calculate_irr_return_flow/calculate_irr_return_flow_from_aqueduct_flood/bash_main_for_all_RCPs.sh
@@ -1,28 +1,28 @@
 
 
 bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 gfdl-esm2m     gfdl-esm2m
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 hadgem2-es     hadgem2-es
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 ipsl-cm5a-lr   ipsl-cm5a-lr
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 miroc-esm-chem miroc-esm-chem
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 noresm1-m      noresm1-m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 hadgem2-es     hadgem2-es
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 ipsl-cm5a-lr   ipsl-cm5a-lr
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 miroc-esm-chem miroc-esm-chem
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp2p6 noresm1-m      noresm1-m
 
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 gfdl-esm2m     gfdl-esm2m
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 hadgem2-es     hadgem2-es
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 ipsl-cm5a-lr   ipsl-cm5a-lr
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 miroc-esm-chem miroc-esm-chem
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 noresm1-m      noresm1-m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 gfdl-esm2m     gfdl-esm2m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 hadgem2-es     hadgem2-es
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 ipsl-cm5a-lr   ipsl-cm5a-lr
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 miroc-esm-chem miroc-esm-chem
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp4p5 noresm1-m      noresm1-m
 
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 gfdl-esm2m     gfdl-esm2m
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 hadgem2-es     hadgem2-es
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 ipsl-cm5a-lr   ipsl-cm5a-lr
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 miroc-esm-chem miroc-esm-chem
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 noresm1-m      noresm1-m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 gfdl-esm2m     gfdl-esm2m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 hadgem2-es     hadgem2-es
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 ipsl-cm5a-lr   ipsl-cm5a-lr
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 miroc-esm-chem miroc-esm-chem
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp6p0 noresm1-m      noresm1-m
 
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 gfdl-esm2m     gfdl-esm2m
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 hadgem2-es     hadgem2-es
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 ipsl-cm5a-lr   ipsl-cm5a-lr
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 miroc-esm-chem miroc-esm-chem
-#~ bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 noresm1-m      noresm1-m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 gfdl-esm2m     gfdl-esm2m
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 hadgem2-es     hadgem2-es
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 ipsl-cm5a-lr   ipsl-cm5a-lr
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 miroc-esm-chem miroc-esm-chem
+bash calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh rcp8p5 noresm1-m      noresm1-m
 
 
 
