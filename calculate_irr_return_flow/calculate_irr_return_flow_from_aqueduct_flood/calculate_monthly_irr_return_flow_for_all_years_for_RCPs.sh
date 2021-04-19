1777dcef854edfc017636b52d6a470eab4020cac
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-04-19 14:12:30 +0200

    calculate irr return flow from aqueduct flood for edward

---
+++ calculate_irr_return_flow/calculate_irr_return_flow_from_aqueduct_flood/calculate_monthly_irr_return_flow_for_all_years_for_RCPs.sh
@@ -0,0 +1,76 @@
+
+RCP_CODE="rcp4p5"
+RCP_CODE=$1
+
+OUTPUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct_for_edward/irrigation_water_use_and_direct_runoff/processed_irrigated/historical/1951-2005/"$1"/"
+
+INPUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct_for_edward/irrigation_water_use_and_direct_runoff/rcp4p5/2006-2099/gfdl-esm2m/"
+INPUT_FOLDER="/scratch-shared/edwinhs/pcr-globwb-aqueduct_for_edward/irrigation_water_use_and_direct_runoff/"${RCP_CODE}"/2006-2099/"$2"/"
+
+
+bash calculate_monthly_irr_return_flow.sh 2006 2006 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2007 2007 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2008 2008 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2009 2009 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2010 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2011 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2012 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2013 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2014 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2015 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2016 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2017 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2018 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2019 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2020 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2021 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2022 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2023 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2024 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2025 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2026 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2027 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2028 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2029 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2030 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2031 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2032 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2033 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2034 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2035 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2036 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2037 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2038 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2039 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2040 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2041 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2042 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2043 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2044 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2045 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2046 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2047 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2048 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2049 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+bash calculate_monthly_irr_return_flow.sh 2050 2010 ${OUTPUT_FOLDER} ${INPUT_FOLDER} &
+
+wait
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
