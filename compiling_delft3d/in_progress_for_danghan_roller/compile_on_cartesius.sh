d2d2a1dc46d88a168f7d1b73466b588c4f919128
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-05-11 23:17:56 +0200

    compiling for danghan (roller)

--- compiling_delft3d/in_progress_for_danghan_roller/compile_on_cartesius.sh
+++ compiling_delft3d/in_progress_for_danghan_roller/compile_on_cartesius.sh
@@ -10,11 +10,11 @@ SVN_USER_NAME="hsutanudjajacchms99"
 # - (deltares) delft3d svn site
 SVN_SRC_SITE="https://svn.oss.deltares.nl/repos/delft3d/tags/delft3d4/7545/"
 # - folder where our own delft3d source version (e.g. from Marcio) is located
-OWN_SRC_FOLDER="/home/edwinhs/Downloads/mk/"
+OWN_SRC_FOLDER="/home/edwinhs/Downloads/mk/src_roller/"
 # - source code folder (relative to MY_WORKING_FOLDER), where our own delft3d source version will be integrated to the one from the (deltares) delft3d svn site  
-SOURCE_FOLDER="src_roller/"
+SOURCE_FOLDER="delft3d_source/"
 # - target folder where delft3d will be installed
-TARGET_INST_FOLDER="/scratch-shared/edwinhs/delft3d_for_danghan_roller_compiled_first_test/"
+TARGET_INST_FOLDER="/scratch-shared/edwinhs/delft3d_for_danghan_roller_compiled_on_20210511/"
 
 
 # get the current directory
@@ -26,24 +26,31 @@ ORIGIN_FOLDER=$(pwd)
 mkdir -p ${MY_WORKING_FOLDER}
 cd ${MY_WORKING_FOLDER}
 pwd
-# - delete the content of temporary folder (only if you like)
-rm -rf *
+
+#~ # - delete the content of temporary folder (only if you like) - THIS IS DANGEROUS
+#~ rm -rf *
+
 
 # obtain the delft3d from the Deltares site
 svn --username ${SVN_USER_NAME} checkout ${SVN_SRC_SITE} ${SOURCE_FOLDER}
 
+#~ # obtain the delft3d from the Deltares site (using export)
+#~ svn --username ${SVN_USER_NAME} export ${SVN_SRC_SITE} ${SOURCE_FOLDER}
+
 
 # add and replace with the ones from our own delft3d source version (e.g. from Marcio) 
 cd ${OWN_SRC_FOLDER}
-mkdir -p ${MY_WORKING_FOLDER}/${SOURCE_FOLDER}/src/
+ls
+ls ${MY_WORKING_FOLDER}/${SOURCE_FOLDER}/src/
 rsync --recursive --verbose --no-p --no-g --chmod=ugo=rwX --size-only --progress * ${MY_WORKING_FOLDER}/${SOURCE_FOLDER}/src/
 
 
 # go to the main source code folder
 cd ${MY_WORKING_FOLDER}/${SOURCE_FOLDER}/src/
 
-#~ # (try to) make sure that we have correct permissions to the source code files - NOT NEEDED
-#~ # chmod -R 755 .
+
+# (try to) make sure that we have correct permissions to the source code files - PERHAPS THIS IS NOT NEEDED
+chmod -R 755 .
 
 
 # load all required modules
@@ -54,6 +61,7 @@ module load eb
 module load intel/2017b
 module load netCDF/4.4.1.1-intel-2017b
 module load netCDF-Fortran/4.4.4-intel-2017b
+
 
 # set the correct variables
 export FC=mpiifort
@@ -75,10 +83,11 @@ chmod 755 clean.sh
 chmod 755 autogen.sh
 ./autogen.sh
 
-#~ # do we need this???
-#~ cd third_party_open/kdtree2
-#~ ./autogen.sh
-#~ cd -
+
+# do we need this???
+cd third_party_open/kdtree2
+./autogen.sh
+cd -
 
 
 # configure
