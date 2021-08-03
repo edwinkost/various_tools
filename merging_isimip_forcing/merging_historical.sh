8cd58f05c19eef1f958d55b81ff7420a00d0feb5
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2021-08-03 15:26:06 +0200

    merging isimip forcing test

--- merging_isimip_forcing/merging_historical.sh
+++ merging_isimip_forcing/merging_historical.sh
@@ -1,35 +1,27 @@
 
-GCM_CODE_CAPITAL="GFDL-ESM4"
-GCM_CODE_SMALL="gfdl-esm4"
-
-SOURCE_DIR="/scratch/depfg/sutan101/data/isimip_forcing/isimip3b_version_2021-05-XX/copied_on_2021-06-XX/source/historical/"${GCM_CODE_CAPITAL}"/"
-OUTPUT_DIR="/scratch/depfg/sutan101/data/isimip_forcing/isimip3b_version_2021-05-XX/copied_on_2021-06-XX/merged/historical/"${GCM_CODE_SMALL}"/"
-
-#~ -r--r--r-- 1 sutan101 depfg 957M Jun 29 22:46 gfdl-esm4_r1i1p1f1_w5e5_historical_hurs_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 29 22:57 gfdl-esm4_r1i1p1f1_w5e5_historical_huss_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 790M Jun 29 23:08 gfdl-esm4_r1i1p1f1_w5e5_historical_pr_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 289M Jun 29 23:12 gfdl-esm4_r1i1p1f1_w5e5_historical_prsn_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 805M Jun 29 23:22 gfdl-esm4_r1i1p1f1_w5e5_historical_ps_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 938M Jun 29 23:36 gfdl-esm4_r1i1p1f1_w5e5_historical_rlds_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 965M Jun 29 23:49 gfdl-esm4_r1i1p1f1_w5e5_historical_rsds_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 30 00:03 gfdl-esm4_r1i1p1f1_w5e5_historical_sfcwind_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 787M Jun 30 00:14 gfdl-esm4_r1i1p1f1_w5e5_historical_tas_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 791M Jun 30 00:24 gfdl-esm4_r1i1p1f1_w5e5_historical_tasmax_global_daily_2011_2014.nc
-#~ -r--r--r-- 1 sutan101 depfg 795M Jun 30 00:35 gfdl-esm4_r1i1p1f1_w5e5_historical_tasmin_global_daily_2011_2014.nc
-
-mkdir -p ${OUTPUT_DIR}
-cd ${OUTPUT_DIR}
-
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_hurs_global_daily_*.nc    ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_hurs_global_daily_1850_2014.nc    &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_huss_global_daily_*.nc    ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_huss_global_daily_1850_2014.nc    &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_pr_global_daily_*.nc      ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_pr_global_daily_1850_2014.nc      &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_prsn_global_daily_*.nc    ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_prsn_global_daily_1850_2014.nc    &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_ps_global_daily_*.nc      ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_ps_global_daily_1850_2014.nc      &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_rlds_global_daily_*.nc    ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_rlds_global_daily_1850_2014.nc    &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_rsds_global_daily_*.nc    ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_rsds_global_daily_1850_2014.nc    &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_sfcwind_global_daily_*.nc ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_sfcwind_global_daily_1850_2014.nc &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tas_global_daily_*.nc     ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tas_global_daily_1850_2014.nc     &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tasmax_global_daily_*.nc  ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tasmax_global_daily_1850_2014.nc  &
-cdo -L -f nc4 -mergetime ${SOURCE_DIR}/${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tasmin_global_daily_*.nc  ${GCM_CODE_SMALL}_r1i1p1f1_w5e5_historical_tasmin_global_daily_1850_2014.nc  &
+bash merging_historical_per_gcm.sh GFDL-ESM4     gfdl-esm4     &
+bash merging_historical_per_gcm.sh IPSL-CM6A-LR  ipsl-cm6a-lr  &
+bash merging_historical_per_gcm.sh MPI-ESM1-2-HR mpi-esm1-2-hr &
+bash merging_historical_per_gcm.sh MRI-ESM2-0    mri-esm2-0    &
+bash merging_historical_per_gcm.sh UKESM1-0-LL   ukesm1-0-ll   &
 
 wait
+
+#~ sutan101@gpu038.cluster:/scratch/depfg/sutan101/data/isimip_forcing/isimip3b_version_2021-05-XX/copied_on_2021-06-XX/source/historical$ ls -lah
+#~ total 3.5K
+#~ dr-xr-xr-x 7 sutan101 depfg   5 Jun 29 22:39 .
+#~ dr-xr-xr-x 6 sutan101 depfg   9 Aug  3 14:46 ..
+#~ dr-xr-xr-x 2 sutan101 depfg 198 Jun 30 00:35 GFDL-ESM4
+#~ dr-xr-xr-x 2 sutan101 depfg 198 Jun 30 02:34 IPSL-CM6A-LR
+#~ dr-xr-xr-x 2 sutan101 depfg 198 Jun 30 04:32 MPI-ESM1-2-HR
+#~ dr-xr-xr-x 2 sutan101 depfg 198 Jun 30 06:34 MRI-ESM2-0
+#~ dr-xr-xr-x 2 sutan101 depfg 198 Jun 30 08:11 UKESM1-0-LL
+
+#~ sutan101@gpu038.cluster:/scratch/depfg/sutan101/data/isimip_forcing/isimip3b_version_2021-05-XX/copied_on_2021-06-XX/source/historical$ ls -lah ls -lah */*huss*2014*
+#~ ls: cannot access ls: No such file or directory
+#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 29 22:57 GFDL-ESM4/gfdl-esm4_r1i1p1f1_w5e5_historical_huss_global_daily_2011_2014.nc
+#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 30 01:01 IPSL-CM6A-LR/ipsl-cm6a-lr_r1i1p1f1_w5e5_historical_huss_global_daily_2011_2014.nc
+#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 30 02:59 MPI-ESM1-2-HR/mpi-esm1-2-hr_r1i1p1f1_w5e5_historical_huss_global_daily_2011_2014.nc
+#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 30 04:58 MRI-ESM2-0/mri-esm2-0_r1i1p1f1_w5e5_historical_huss_global_daily_2011_2014.nc
+#~ -r--r--r-- 1 sutan101 depfg 1.1G Jun 30 06:52 UKESM1-0-LL/ukesm1-0-ll_r1i1p1f2_w5e5_historical_huss_global_daily_2011_2014.nc
+
