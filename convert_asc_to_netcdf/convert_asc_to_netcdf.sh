5b614006a94e84172d4934ba54006f701ed5dd44
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-08-12 11:12:32 +0200

    convert asc to nc

--- convert_asc_to_netcdf/convert_asc_to_netcdf.sh
+++ convert_asc_to_netcdf/convert_asc_to_netcdf.sh
@@ -1,10 +1,11 @@
 
 INP_FILE=$1
 OUT_FILE=$2
-DATETIME=$3
+
+DATE=$3
 
 gdal_translate -of NETCDF ${INP_FILE} ${OUT_FILE}.tmp
-cdo -L -f nc4 -setreftime, -setdate,${DATETIME} ${OUT_FILE}.tmp ${OUT_FILE} 
+cdo -L -f nc4 -setreftime,1850-01-01,00:00:00 -settime,00:00:00 -setdate,${DATE} ${OUT_FILE}.tmp ${OUT_FILE} 
 rm ${OUT_FILE}.tmp
 
 
