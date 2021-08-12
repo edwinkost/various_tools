42d8605901a4e9a1df6972ce7839b08c3a32c374
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-08-12 11:42:36 +0200

    process hyde irrigation maps, including invertlat

--- convert_asc_to_netcdf/convert_asc_to_netcdf.sh
+++ convert_asc_to_netcdf/convert_asc_to_netcdf.sh
@@ -5,7 +5,7 @@ OUT_FILE=$2
 DATE=$3
 
 gdal_translate -of NETCDF ${INP_FILE} ${OUT_FILE}.tmp
-cdo -L -f nc4 -setreftime,1850-01-01,00:00:00 -settime,00:00:00 -setdate,${DATE} ${OUT_FILE}.tmp ${OUT_FILE} 
+cdo -L -f nc4 -setreftime,1850-01-01,00:00:00 -settime,00:00:00 -setdate,${DATE} -invertlat ${OUT_FILE}.tmp ${OUT_FILE} 
 rm ${OUT_FILE}.tmp
 
 
