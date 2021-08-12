458b5f66515d312da15cff996f19609cc2235e6e
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-08-12 11:21:43 +0200

    convert asc to nc

--- convert_asc_to_netcdf/convert_hyde3p2_irrigation.sh
+++ convert_asc_to_netcdf/convert_hyde3p2_irrigation.sh
@@ -146,5 +146,47 @@ bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_norice2017AD.asc ${OUT_FOLDER}/ir
 
 wait
 
+# tot_irri (total irrigation), e.g. tot_irri2017AD.asc
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1800AD.asc ${OUT_FOLDER}/tot_irri1800AD.nc "1800-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1810AD.asc ${OUT_FOLDER}/tot_irri1810AD.nc "1810-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1820AD.asc ${OUT_FOLDER}/tot_irri1820AD.nc "1820-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1830AD.asc ${OUT_FOLDER}/tot_irri1830AD.nc "1830-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1840AD.asc ${OUT_FOLDER}/tot_irri1840AD.nc "1840-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1850AD.asc ${OUT_FOLDER}/tot_irri1850AD.nc "1850-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1860AD.asc ${OUT_FOLDER}/tot_irri1860AD.nc "1860-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1870AD.asc ${OUT_FOLDER}/tot_irri1870AD.nc "1870-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1880AD.asc ${OUT_FOLDER}/tot_irri1880AD.nc "1880-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1890AD.asc ${OUT_FOLDER}/tot_irri1890AD.nc "1890-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1900AD.asc ${OUT_FOLDER}/tot_irri1900AD.nc "1900-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1910AD.asc ${OUT_FOLDER}/tot_irri1910AD.nc "1910-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1920AD.asc ${OUT_FOLDER}/tot_irri1920AD.nc "1920-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1930AD.asc ${OUT_FOLDER}/tot_irri1930AD.nc "1930-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1940AD.asc ${OUT_FOLDER}/tot_irri1940AD.nc "1940-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1950AD.asc ${OUT_FOLDER}/tot_irri1950AD.nc "1950-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1960AD.asc ${OUT_FOLDER}/tot_irri1960AD.nc "1960-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1970AD.asc ${OUT_FOLDER}/tot_irri1970AD.nc "1970-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1980AD.asc ${OUT_FOLDER}/tot_irri1980AD.nc "1980-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri1990AD.asc ${OUT_FOLDER}/tot_irri1990AD.nc "1990-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2000AD.asc ${OUT_FOLDER}/tot_irri2000AD.nc "2000-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2001AD.asc ${OUT_FOLDER}/tot_irri2001AD.nc "2001-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2002AD.asc ${OUT_FOLDER}/tot_irri2002AD.nc "2002-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2003AD.asc ${OUT_FOLDER}/tot_irri2003AD.nc "2003-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2004AD.asc ${OUT_FOLDER}/tot_irri2004AD.nc "2004-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2005AD.asc ${OUT_FOLDER}/tot_irri2005AD.nc "2005-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2006AD.asc ${OUT_FOLDER}/tot_irri2006AD.nc "2006-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2007AD.asc ${OUT_FOLDER}/tot_irri2007AD.nc "2007-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2008AD.asc ${OUT_FOLDER}/tot_irri2008AD.nc "2008-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2009AD.asc ${OUT_FOLDER}/tot_irri2009AD.nc "2009-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2010AD.asc ${OUT_FOLDER}/tot_irri2010AD.nc "2010-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2011AD.asc ${OUT_FOLDER}/tot_irri2011AD.nc "2011-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2012AD.asc ${OUT_FOLDER}/tot_irri2012AD.nc "2012-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2013AD.asc ${OUT_FOLDER}/tot_irri2013AD.nc "2013-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2014AD.asc ${OUT_FOLDER}/tot_irri2014AD.nc "2014-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2015AD.asc ${OUT_FOLDER}/tot_irri2015AD.nc "2015-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2016AD.asc ${OUT_FOLDER}/tot_irri2016AD.nc "2016-01-01" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/tot_irri2017AD.asc ${OUT_FOLDER}/tot_irri2017AD.nc "2017-01-01" &
+
+wait
+
 
 set +x
