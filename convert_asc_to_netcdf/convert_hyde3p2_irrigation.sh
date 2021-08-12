87d8fd254ed17e17146dd508cdecbd74646be0c4
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-08-12 11:09:09 +0200

    convert asc to nc

--- convert_asc_to_netcdf/convert_hyde3p2_irrigation.sh
+++ convert_asc_to_netcdf/convert_hyde3p2_irrigation.sh
@@ -63,44 +63,44 @@ mkdir -p ${OUT_FOLDER}
 
 
 # irrigation rice (paddy)
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1800AD.asc ${OUT_FOLDER}/ir_rice1800AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1810AD.asc ${OUT_FOLDER}/ir_rice1810AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1820AD.asc ${OUT_FOLDER}/ir_rice1820AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1830AD.asc ${OUT_FOLDER}/ir_rice1830AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1840AD.asc ${OUT_FOLDER}/ir_rice1840AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1850AD.asc ${OUT_FOLDER}/ir_rice1850AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1860AD.asc ${OUT_FOLDER}/ir_rice1860AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1870AD.asc ${OUT_FOLDER}/ir_rice1870AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1880AD.asc ${OUT_FOLDER}/ir_rice1880AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1890AD.asc ${OUT_FOLDER}/ir_rice1890AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1900AD.asc ${OUT_FOLDER}/ir_rice1900AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1910AD.asc ${OUT_FOLDER}/ir_rice1910AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1920AD.asc ${OUT_FOLDER}/ir_rice1920AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1930AD.asc ${OUT_FOLDER}/ir_rice1930AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1940AD.asc ${OUT_FOLDER}/ir_rice1940AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1950AD.asc ${OUT_FOLDER}/ir_rice1950AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1960AD.asc ${OUT_FOLDER}/ir_rice1960AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1970AD.asc ${OUT_FOLDER}/ir_rice1970AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1980AD.asc ${OUT_FOLDER}/ir_rice1980AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1990AD.asc ${OUT_FOLDER}/ir_rice1990AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2000AD.asc ${OUT_FOLDER}/ir_rice2000AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2001AD.asc ${OUT_FOLDER}/ir_rice2001AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2002AD.asc ${OUT_FOLDER}/ir_rice2002AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2003AD.asc ${OUT_FOLDER}/ir_rice2003AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2004AD.asc ${OUT_FOLDER}/ir_rice2004AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2005AD.asc ${OUT_FOLDER}/ir_rice2005AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2006AD.asc ${OUT_FOLDER}/ir_rice2006AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2007AD.asc ${OUT_FOLDER}/ir_rice2007AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2008AD.asc ${OUT_FOLDER}/ir_rice2008AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2009AD.asc ${OUT_FOLDER}/ir_rice2009AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2010AD.asc ${OUT_FOLDER}/ir_rice2010AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2011AD.asc ${OUT_FOLDER}/ir_rice2011AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2012AD.asc ${OUT_FOLDER}/ir_rice2012AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2013AD.asc ${OUT_FOLDER}/ir_rice2013AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2014AD.asc ${OUT_FOLDER}/ir_rice2014AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2015AD.asc ${OUT_FOLDER}/ir_rice2015AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2016AD.asc ${OUT_FOLDER}/ir_rice2016AD.nc &
-bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2017AD.asc ${OUT_FOLDER}/ir_rice2017AD.nc &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1800AD.asc ${OUT_FOLDER}/ir_rice1800AD.nc "1800-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1810AD.asc ${OUT_FOLDER}/ir_rice1810AD.nc "1810-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1820AD.asc ${OUT_FOLDER}/ir_rice1820AD.nc "1820-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1830AD.asc ${OUT_FOLDER}/ir_rice1830AD.nc "1830-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1840AD.asc ${OUT_FOLDER}/ir_rice1840AD.nc "1840-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1850AD.asc ${OUT_FOLDER}/ir_rice1850AD.nc "1850-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1860AD.asc ${OUT_FOLDER}/ir_rice1860AD.nc "1860-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1870AD.asc ${OUT_FOLDER}/ir_rice1870AD.nc "1870-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1880AD.asc ${OUT_FOLDER}/ir_rice1880AD.nc "1880-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1890AD.asc ${OUT_FOLDER}/ir_rice1890AD.nc "1890-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1900AD.asc ${OUT_FOLDER}/ir_rice1900AD.nc "1900-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1910AD.asc ${OUT_FOLDER}/ir_rice1910AD.nc "1910-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1920AD.asc ${OUT_FOLDER}/ir_rice1920AD.nc "1920-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1930AD.asc ${OUT_FOLDER}/ir_rice1930AD.nc "1930-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1940AD.asc ${OUT_FOLDER}/ir_rice1940AD.nc "1940-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1950AD.asc ${OUT_FOLDER}/ir_rice1950AD.nc "1950-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1960AD.asc ${OUT_FOLDER}/ir_rice1960AD.nc "1960-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1970AD.asc ${OUT_FOLDER}/ir_rice1970AD.nc "1970-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1980AD.asc ${OUT_FOLDER}/ir_rice1980AD.nc "1980-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice1990AD.asc ${OUT_FOLDER}/ir_rice1990AD.nc "1990-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2000AD.asc ${OUT_FOLDER}/ir_rice2000AD.nc "2000-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2001AD.asc ${OUT_FOLDER}/ir_rice2001AD.nc "2001-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2002AD.asc ${OUT_FOLDER}/ir_rice2002AD.nc "2002-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2003AD.asc ${OUT_FOLDER}/ir_rice2003AD.nc "2003-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2004AD.asc ${OUT_FOLDER}/ir_rice2004AD.nc "2004-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2005AD.asc ${OUT_FOLDER}/ir_rice2005AD.nc "2005-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2006AD.asc ${OUT_FOLDER}/ir_rice2006AD.nc "2006-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2007AD.asc ${OUT_FOLDER}/ir_rice2007AD.nc "2007-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2008AD.asc ${OUT_FOLDER}/ir_rice2008AD.nc "2008-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2009AD.asc ${OUT_FOLDER}/ir_rice2009AD.nc "2009-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2010AD.asc ${OUT_FOLDER}/ir_rice2010AD.nc "2010-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2011AD.asc ${OUT_FOLDER}/ir_rice2011AD.nc "2011-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2012AD.asc ${OUT_FOLDER}/ir_rice2012AD.nc "2012-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2013AD.asc ${OUT_FOLDER}/ir_rice2013AD.nc "2013-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2014AD.asc ${OUT_FOLDER}/ir_rice2014AD.nc "2014-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2015AD.asc ${OUT_FOLDER}/ir_rice2015AD.nc "2015-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2016AD.asc ${OUT_FOLDER}/ir_rice2016AD.nc "2016-01-01,00:00:00" &
+bash convert_asc_to_netcdf.sh ${INP_FOLDER}/ir_rice2017AD.asc ${OUT_FOLDER}/ir_rice2017AD.nc "2017-01-01,00:00:00" &
 
 wait
 
