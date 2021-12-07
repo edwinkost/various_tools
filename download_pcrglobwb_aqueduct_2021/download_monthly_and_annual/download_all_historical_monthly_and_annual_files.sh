8c349a75a56ff7510b7c6097e9d264cb38aaa0ff
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-07 10:52:12 +0100

    start downloading historical files

---
+++ download_pcrglobwb_aqueduct_2021/download_monthly_and_annual/download_all_historical_monthly_and_annual_files.sh
@@ -0,0 +1,12 @@
+
+# historical reference
+bash download_monthly_and_annual.sh gswp3-w5e5    historical_reference 1960
+
+# historical
+bash download_monthly_and_annual.sh gfdl-esm4     historical           1960
+bash download_monthly_and_annual.sh ipsl-cm6a-lr  historical           1960
+bash download_monthly_and_annual.sh mpi-esm1-2-hr historical           1960
+bash download_monthly_and_annual.sh mri-esm2-0    historical           1960
+bash download_monthly_and_annual.sh ukesm1-0-ll   historical           1960
+
+
---
+++ download_pcrglobwb_aqueduct_2021/download_monthly_and_annual/download_monthly_and_annual.sh
@@ -0,0 +1,30 @@
+
+# first you have to start azcopy and login
+
+
+#~ GCM_NAME="gfdl-esm4"
+#~ SCENARIO_TYPE="historical"
+#~ STARTING_YEAR="1960"
+
+
+# please provide the following variables
+GCM_NAME=$1
+SCENARIO_TYPE=$2
+STARTING_YEAR=S3
+
+
+# source and target folders
+SOURCE_FOLDER="https://uuwridata.dfs.core.windows.net/pgb-data-lake/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/netcdf"
+TARGET_FOLDER="/projects/0/dfguu2/users/edwin/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/"
+
+
+# go to the TARGET_FOLDER and create a "netcdf" directory and go there
+cd ${TARGET_FOLDER}
+mkdir netcdf
+cd netcdf
+
+
+# download using azcopy
+echo azcopy sync ${SOURCE_FOLDER} "."
+azcopy sync ${SOURCE_FOLDER} "."
+
