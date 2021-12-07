8c6f146ca3a8720f3e3c245e9ef4c79a7e9fea5c
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-07 15:42:41 +0100

    upload daily files of pcrglobwb aqueduct 2021 - historical

---
+++ upload_with_azcopy/upload_all_historical_daily_files_aqueduct_2021.sh
@@ -0,0 +1,6 @@
+
+bash upload_daily_files_aqueduct_2021.sh gfdl-esm4     historical 1960
+bash upload_daily_files_aqueduct_2021.sh ipsl-cm6a-lr  historical 1960
+bash upload_daily_files_aqueduct_2021.sh mpi-esm1-2-hr historical 1960
+bash upload_daily_files_aqueduct_2021.sh mri-esm2-0    historical 1960
+bash upload_daily_files_aqueduct_2021.sh ukesm1-0-ll   historical 1960
---
+++ upload_with_azcopy/upload_daily_files_aqueduct_2021.sh
@@ -0,0 +1,45 @@
+
+# first you have to start azcopy and login
+
+unset GCM_NAME
+unset SCENARIO_TYPE
+unset STARTING_YEAR
+unset LOCAL_FOLDER
+unset DATALAKE_FOLDER
+
+GCM_NAME="gfdl-esm4"
+SCENARIO_TYPE="ssp585"
+STARTING_YEAR="2015"
+
+GCM_NAME="gfdl-esm4"
+SCENARIO_TYPE="historical"
+STARTING_YEAR="1960"
+
+
+# please provide the following variables
+GCM_NAME=$1
+SCENARIO_TYPE=$2
+STARTING_YEAR=S3
+
+
+# local/source folder
+LOCAL_FOLDER="/gcm_disk*/*/pcrglobwb/merged_daily/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/*/global"
+
+# data lake location/folder
+DATALAKE_FOLDER="https://uuwridata.dfs.core.windows.net/pgb-data-lake/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global"
+
+
+# go to the local/source folder and make the signature of it
+cd ${LOCAL_FOLDER}
+cd netcdf_daily
+pwd > 000_source.txt
+
+# go to one folder up
+cd ..
+pwd > source_daily_files.txt
+
+
+# upload using azcopy sync
+echo azcopy sync "." ${DATALAKE_FOLDER}
+azcopy sync "." ${DATALAKE_FOLDER}
+
