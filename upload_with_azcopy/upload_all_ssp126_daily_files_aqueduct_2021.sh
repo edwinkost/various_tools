311e7a2d835c0847bbb7de0d6787da9702e66da2
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-07 16:15:38 +0100

    upload daily files of pcrglobwb aqueduct 2021 - ssp126

---
+++ upload_with_azcopy/upload_all_ssp126_daily_files_aqueduct_2021.sh
@@ -0,0 +1,6 @@
+
+bash upload_daily_files_aqueduct_2021.sh gfdl-esm4     ssp126 2015
+bash upload_daily_files_aqueduct_2021.sh ipsl-cm6a-lr  ssp126 2015
+bash upload_daily_files_aqueduct_2021.sh mpi-esm1-2-hr ssp126 2015
+bash upload_daily_files_aqueduct_2021.sh mri-esm2-0    ssp126 2015
+bash upload_daily_files_aqueduct_2021.sh ukesm1-0-ll   ssp126 2015
