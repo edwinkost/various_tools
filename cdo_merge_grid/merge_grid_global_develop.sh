a4d05247bc8d58fb657616f5e3d4a23cd5b84aef
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-12 22:19:08 +0100

    test merge grid

--- cdo_merge_grid/merge_grid_global_develop.sh
+++ cdo_merge_grid/merge_grid_global_develop.sh
@@ -6,11 +6,13 @@ MAIN_SOURCE_FOLDER="/scratch/depfg/sutan101/land_covers_global/version_2021-02-X
 MAIN_OUTPUT_FOLDER="/scratch/depfg/sutan101/land_covers_global/version_2021-02-XX/merged/"
 
 # clean output folder
+mkdir -p ${MAIN_OUTPUT_FOLDER}
 rm ${MAIN_OUTPUT_FOLDER}
 
-FILENAME="rfrac1_all.nc"
 
-#~ FILENAME=$1
+FILENAME="rfrac1_all.nc"
+FILENAME=$1
+
 
 GRIDDES_FILE="/scratch/depfg/sutan101/data/pcrglobwb_input_arise/develop/global_30sec/cloneMaps/griddes_global_30sec_clone_correct_lat.nc.txt"
 
@@ -26,7 +28,9 @@ cdo remapcon,${GRIDDES_FILE} ${SOURCE_FOLDER}/${FILENAME} ${MAIN_OUTPUT_FOLDER}/
 
 # remapcon and mergegrid to the files 2 to 16
 
-for i in {2..16}
+#~ for i in {2..16}
+
+for i in {2..3}
 
 do
 
