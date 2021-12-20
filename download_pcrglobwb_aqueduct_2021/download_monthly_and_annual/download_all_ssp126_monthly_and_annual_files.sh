4f68c4aea8c20691f04a46727cacdc210649675e
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-20 22:40:22 +0100

    download ssp126

---
+++ download_pcrglobwb_aqueduct_2021/download_monthly_and_annual/download_all_ssp126_monthly_and_annual_files.sh
@@ -0,0 +1,8 @@
+
+bash download_monthly_and_annual.sh gfdl-esm4     ssp126 2015
+bash download_monthly_and_annual.sh ipsl-cm6a-lr  ssp126 2015
+bash download_monthly_and_annual.sh mpi-esm1-2-hr ssp126 2015
+bash download_monthly_and_annual.sh mri-esm2-0    ssp126 2015
+bash download_monthly_and_annual.sh ukesm1-0-ll   ssp126 2015
+
+
