17368db7501b0bb0979c55377bec24d64f73b91a
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2021-12-20 22:12:59 +0100

    download ssp370

---
+++ download_pcrglobwb_aqueduct_2021/download_monthly_and_annual/download_all_ssp370_monthly_and_annual_files.sh
@@ -0,0 +1,8 @@
+
+bash download_monthly_and_annual.sh gfdl-esm4     ssp370 2015
+bash download_monthly_and_annual.sh ipsl-cm6a-lr  ssp370 2015
+bash download_monthly_and_annual.sh mpi-esm1-2-hr ssp370 2015
+bash download_monthly_and_annual.sh mri-esm2-0    ssp370 2015
+bash download_monthly_and_annual.sh ukesm1-0-ll   ssp370 2015
+
+
