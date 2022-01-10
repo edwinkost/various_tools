e9848ed44e53ca26084b4c587243d468199bb55d
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-10 10:33:19 +0100

    submit all_jobs_for_rcps 4p5 6p0 8p5

---
+++ cdo_tim_operation/all_jobs_for_rcp4p5_rcp6p0_rcp8p5.sh
@@ -0,0 +1,34 @@
+
+
+# rcp4p5
+
+MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge/rcp4p5/"
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge_statistics/rcp4p5/"
+
+bash all_jobs_flexible_template.sh 4p5g rcp4p5 gfdl-esm2m     ${MAIN_INP_FOLDER}/2006-2099/gfdl-esm2m/     ${MAIN_OUT_FOLDER}/2006-2099/gfdl-esm2m/    
+bash all_jobs_flexible_template.sh 4p5h rcp4p5 hadgem2-es     ${MAIN_INP_FOLDER}/2006-2099/hadgem2-es/     ${MAIN_OUT_FOLDER}/2006-2099/hadgem2-es/    
+bash all_jobs_flexible_template.sh 4p5i rcp4p5 ipsl-cm5a-lr   ${MAIN_INP_FOLDER}/2006-2099/ipsl-cm5a-lr/   ${MAIN_OUT_FOLDER}/2006-2099/ipsl-cm5a-lr/  
+bash all_jobs_flexible_template.sh 4p5m rcp4p5 miroc-esm-chem ${MAIN_INP_FOLDER}/2006-2099/miroc-esm-chem/ ${MAIN_OUT_FOLDER}/2006-2099/miroc-esm-chem/
+bash all_jobs_flexible_template.sh 4p5n rcp4p5 noresm1-m      ${MAIN_INP_FOLDER}/2006-2099/noresm1-m/      ${MAIN_OUT_FOLDER}/2006-2099/noresm1-m/     
+
+# rcp6p0
+
+MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge/rcp6p0/"
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge_statistics/rcp6p0/"
+
+bash all_jobs_flexible_template.sh 6p0g rcp6p0 gfdl-esm2m     ${MAIN_INP_FOLDER}/2006-2099/gfdl-esm2m/     ${MAIN_OUT_FOLDER}/2006-2099/gfdl-esm2m/    
+bash all_jobs_flexible_template.sh 6p0h rcp6p0 hadgem2-es     ${MAIN_INP_FOLDER}/2006-2099/hadgem2-es/     ${MAIN_OUT_FOLDER}/2006-2099/hadgem2-es/    
+bash all_jobs_flexible_template.sh 6p0i rcp6p0 ipsl-cm5a-lr   ${MAIN_INP_FOLDER}/2006-2099/ipsl-cm5a-lr/   ${MAIN_OUT_FOLDER}/2006-2099/ipsl-cm5a-lr/  
+bash all_jobs_flexible_template.sh 6p0m rcp6p0 miroc-esm-chem ${MAIN_INP_FOLDER}/2006-2099/miroc-esm-chem/ ${MAIN_OUT_FOLDER}/2006-2099/miroc-esm-chem/
+bash all_jobs_flexible_template.sh 6p0n rcp6p0 noresm1-m      ${MAIN_INP_FOLDER}/2006-2099/noresm1-m/      ${MAIN_OUT_FOLDER}/2006-2099/noresm1-m/     
+
+# rcp8p5
+
+MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge/rcp8p5/"
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge_statistics/rcp8p5/"
+
+bash all_jobs_flexible_template.sh 8p5g rcp8p5 gfdl-esm2m     ${MAIN_INP_FOLDER}/2006-2099/gfdl-esm2m/     ${MAIN_OUT_FOLDER}/2006-2099/gfdl-esm2m/    
+bash all_jobs_flexible_template.sh 8p5h rcp8p5 hadgem2-es     ${MAIN_INP_FOLDER}/2006-2099/hadgem2-es/     ${MAIN_OUT_FOLDER}/2006-2099/hadgem2-es/    
+bash all_jobs_flexible_template.sh 8p5i rcp8p5 ipsl-cm5a-lr   ${MAIN_INP_FOLDER}/2006-2099/ipsl-cm5a-lr/   ${MAIN_OUT_FOLDER}/2006-2099/ipsl-cm5a-lr/  
+bash all_jobs_flexible_template.sh 8p5m rcp8p5 miroc-esm-chem ${MAIN_INP_FOLDER}/2006-2099/miroc-esm-chem/ ${MAIN_OUT_FOLDER}/2006-2099/miroc-esm-chem/
+bash all_jobs_flexible_template.sh 8p5n rcp8p5 noresm1-m      ${MAIN_INP_FOLDER}/2006-2099/noresm1-m/      ${MAIN_OUT_FOLDER}/2006-2099/noresm1-m/     
