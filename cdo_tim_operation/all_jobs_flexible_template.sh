898ded8a5e19c3ca26d225a83ac598eac21fdc9f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-10 09:48:38 +0100

    submit all_jobs_for_historical.sh

---
+++ cdo_tim_operation/all_jobs_flexible_template.sh
@@ -0,0 +1,39 @@
+
+# historical
+
+MAIN_JOB_NAME="hisw" 
+
+RCP_CODE="historical"
+GCM_CODE="watch"
+
+INP_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge/historical/1958-2001_watch/watch/"
+OUT_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge_statistics/historical/1958-2001_watch/watch/"
+
+STA_YEAR=2000
+END_YEAR=2000
+
+MAIN_JOB_NAME=$1 
+RCP_CODE=$2
+GCM_CODE=$3
+
+INP_FOLDER=$4
+OUT_FOLDER=$5
+
+STA_YEAR=$(($6))
+END_YEAR=$(($7))
+
+for i in {${STA_YEAR}..${END_YEAR}}
+
+do
+
+JOB_NAME=${MAIN_JOB_NAME}${i}
+
+sbatch -J ${JOB_NAME} --export INP_FOLDER=${INP_FOLDER},OUT_FOLDER=${OUT_FOLDER},RCP_CODE=${RCP_CODE},GCM_CODE=${GCM_CODE},YEAR=${i} a_single_job_cdo_tim_pctl.sh 
+ 
+done
+
+#~ -r--r--r-- 1 edwinhs edwinhs 4399163672 Jan  6 23:25 rcp2p6/2006-2099/gfdl-esm2m/discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4407988128 Jan  6 22:13 rcp2p6/2006-2099/hadgem2-es/discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4360588471 Jan  6 23:26 rcp2p6/2006-2099/ipsl-cm5a-lr/discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4304269057 Jan  6 20:27 rcp2p6/2006-2099/miroc-esm-chem/discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4322008939 Jan  7 00:50 rcp2p6/2006-2099/noresm1-m/discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
---
+++ cdo_tim_operation/all_jobs_for_historical.sh
@@ -0,0 +1,15 @@
+
+MAIN_INP_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge/historical/"
+MAIN_OUT_FOLDER="/scratch-shared/edwinhs/pcrglobwb_aqueduct_version_2016-2018/aqueduct_daily_discharge_statistics/historical/"
+
+bash all_jobs_flexible_template.sh hisg historical gfdl-esm2m     ${MAIN_INP_FOLDER}/1951-2005/gfdl-esm2m/     ${MAIN_OUT_FOLDER}/1951-2005/gfdl-esm2m/     1951 2005
+bash all_jobs_flexible_template.sh hish historical hadgem2-es     ${MAIN_INP_FOLDER}/1951-2005/hadgem2-es/     ${MAIN_OUT_FOLDER}/1951-2005/hadgem2-es/     1951 2005
+bash all_jobs_flexible_template.sh hisi historical ipsl-cm5a-lr   ${MAIN_INP_FOLDER}/1951-2005/ipsl-cm5a-lr/   ${MAIN_OUT_FOLDER}/1951-2005/ipsl-cm5a-lr/   1951 2005
+bash all_jobs_flexible_template.sh hism historical miroc-esm-chem ${MAIN_INP_FOLDER}/1951-2005/miroc-esm-chem/ ${MAIN_OUT_FOLDER}/1951-2005/miroc-esm-chem/ 1951 2005
+bash all_jobs_flexible_template.sh hisn historical noresm1-m      ${MAIN_INP_FOLDER}/1951-2005/noresm1-m/      ${MAIN_OUT_FOLDER}/1951-2005/noresm1-m/      1951 2005
+
+#~ -r--r--r-- 1 edwinhs edwinhs 4399163672 Jan  6 23:25 rcp2p6/2006-2099/gfdl-esm2m     /discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4407988128 Jan  6 22:13 rcp2p6/2006-2099/hadgem2-es     /discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4360588471 Jan  6 23:26 rcp2p6/2006-2099/ipsl-cm5a-lr   /discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4304269057 Jan  6 20:27 rcp2p6/2006-2099/miroc-esm-chem /discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
+#~ -r--r--r-- 1 edwinhs edwinhs 4322008939 Jan  7 00:50 rcp2p6/2006-2099/noresm1-m      /discharge_dailyTot_output_2099-01-01_to_2099-12-31.nc
