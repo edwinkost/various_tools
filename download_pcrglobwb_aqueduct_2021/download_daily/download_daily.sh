9a68e6fb09509a1ab5841dcce382cc57b09930d6
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-27 17:06:49 +0100

    download daily ipsl-cm6a-lr

--- download_pcrglobwb_aqueduct_2021/download_daily/download_daily.sh
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily.sh
@@ -15,8 +15,9 @@ STARTING_YEAR=$3
 
 # source and target folders
 SOURCE_FOLDER="https://uuwridata.dfs.core.windows.net/pgb-data-lake/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/netcdf_daily"
-TARGET_FOLDER="/gpfs/work4/0/dynql//gpfs/work4/0/dynql/edwindql/pcrglobwb_aqueduct_2021_daily_files/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/"
+TARGET_FOLDER="/gpfs/work4/0/dynql/pcrglobwb_aqueduct_2021_daily_files/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/"
 
+set -x
 
 # go to the TARGET_FOLDER and create a "netcdf" directory and go there
 mkdir -p ${TARGET_FOLDER}
@@ -24,6 +25,7 @@ cd ${TARGET_FOLDER}
 mkdir netcdf
 cd netcdf
 
+set +x
 
 # download using azcopy
 echo azcopy sync ${SOURCE_FOLDER} "."
