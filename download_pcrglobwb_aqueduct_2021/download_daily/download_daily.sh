df7a8f50583ba3647574c96b1da579deda78dafe
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-27 17:08:13 +0100

    download daily ipsl-cm6a-lr

--- download_pcrglobwb_aqueduct_2021/download_daily/download_daily.sh
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily.sh
@@ -15,7 +15,7 @@ STARTING_YEAR=$3
 
 # source and target folders
 SOURCE_FOLDER="https://uuwridata.dfs.core.windows.net/pgb-data-lake/pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/netcdf_daily"
-TARGET_FOLDER="/gpfs/work4/0/dynql/pcrglobwb_aqueduct_2021_daily_files/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/"
+TARGET_FOLDER="/gpfs/work4/0/dynql/edwindql/pcrglobwb_aqueduct_2021_daily_files/version_2021-09-16/"${GCM_NAME}"/"${SCENARIO_TYPE}"/begin_from_"${STARTING_YEAR}"/global/"
 
 set -x
 
