0853e1b6e6f39d9d0edeefe79131210bf8399d1b
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-28 01:11:46 +0100

    download daily per gcm

---
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily_mpi-esm1-2-hr.sh
@@ -0,0 +1,7 @@
+
+bash download_daily.sh mpi-esm1-2-hr historical 1960
+
+bash download_daily.sh mpi-esm1-2-hr ssp126 2015
+bash download_daily.sh mpi-esm1-2-hr ssp370 2015
+bash download_daily.sh mpi-esm1-2-hr ssp585 2015
+
