e5fcd5cc2085484c163e70cc2ceb99f859ad0347
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-28 11:14:53 +0100

    download daily per gcm

---
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily_ukesm1-0-ll.sh
@@ -0,0 +1,7 @@
+
+bash download_daily.sh ukesm1-0-ll historical 1960
+
+bash download_daily.sh ukesm1-0-ll ssp126 2015
+bash download_daily.sh ukesm1-0-ll ssp370 2015
+bash download_daily.sh ukesm1-0-ll ssp585 2015
+
