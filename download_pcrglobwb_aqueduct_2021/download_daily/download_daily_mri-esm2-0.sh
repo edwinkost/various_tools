1fb85eb3b9a797b05ea7e9eb5c8dd6516a765889
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-28 09:46:33 +0100

    download daily per gcm

---
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily_mri-esm2-0.sh
@@ -0,0 +1,7 @@
+
+bash download_daily.sh mri-esm2-0 historical 1960
+
+bash download_daily.sh mri-esm2-0 ssp126 2015
+bash download_daily.sh mri-esm2-0 ssp370 2015
+bash download_daily.sh mri-esm2-0 ssp585 2015
+
