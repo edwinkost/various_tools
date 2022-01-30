d8e3baa183b1d9b6f70ccae0e1de1d2b1615c06a
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-30 06:10:40 +0100

    download daily w5e5

---
+++ download_pcrglobwb_aqueduct_2021/download_daily/download_daily_gswp3-w5e5.sh
@@ -0,0 +1,6 @@
+
+#~ pcrglobwb_output/pcrglobwb_aqueduct_2021/version_2021-09-16/gswp3-w5e5/historical-reference/begin_from_1960/global/netcdf_daily/
+
+bash download_daily.sh gswp3-w5e5 historical-reference 1960
+
+
