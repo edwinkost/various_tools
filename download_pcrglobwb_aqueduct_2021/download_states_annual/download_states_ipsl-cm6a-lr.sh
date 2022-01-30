732784ad34374b2e4b688ca2b99c342034d6d7d5
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-30 08:11:03 +0100

    download states - per gcm - every about 10 year

---
+++ download_pcrglobwb_aqueduct_2021/download_states_annual/download_states_ipsl-cm6a-lr.sh
@@ -0,0 +1,7 @@
+
+bash download_states.sh ipsl-cm6a-lr historical 1960
+
+bash download_states.sh ipsl-cm6a-lr ssp126 2015
+bash download_states.sh ipsl-cm6a-lr ssp370 2015
+bash download_states.sh ipsl-cm6a-lr ssp585 2015
+
