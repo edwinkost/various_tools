bf73c13b1716f95f372aef94c533d5dec8d6a372
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-30 08:08:19 +0100

    download states - per gcm

--- download_pcrglobwb_aqueduct_2021/download_states_annual/download_states_gswp3-w5e5.sh
+++ download_pcrglobwb_aqueduct_2021/download_states_annual/download_states_gswp3-w5e5.sh
@@ -1,5 +1,5 @@
 
-bash download_states.sh gswp3-w5e5       historical_reference 1960
+bash download_states.sh gswp3-w5e5       historical-reference 1960
 
-bash download_states.sh gswp3-w5e5_rerun historical_reference 1960
+bash download_states.sh gswp3-w5e5_rerun historical-reference 1960
 
