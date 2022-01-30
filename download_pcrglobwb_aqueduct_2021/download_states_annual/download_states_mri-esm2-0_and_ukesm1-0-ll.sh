2215c6f3d50798be22a98b091ce6251431f632a5
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2022-01-30 08:21:35 +0100

    download states - per gcm - every about 10 year

---
+++ download_pcrglobwb_aqueduct_2021/download_states_annual/download_states_mri-esm2-0_and_ukesm1-0-ll.sh
@@ -0,0 +1,17 @@
+
+bash download_states.sh mri-esm2-0 historical 1960
+
+bash download_states.sh mri-esm2-0 ssp126 2015
+bash download_states.sh mri-esm2-0 ssp370 2015
+bash download_states.sh mri-esm2-0 ssp585 2015
+
+
+bash download_states.sh ukesm1-0-ll historical 1960
+
+bash download_states.sh ukesm1-0-ll ssp126 2015
+bash download_states.sh ukesm1-0-ll ssp370 2015
+bash download_states.sh ukesm1-0-ll ssp585 2015
+
+
+
+
