9c038f9e106f81864ea012e6a4240f4a59c65784
Edwin Sutanudjaja <edwinkost@UU109053>
2023-01-17 11:17:46 +0100

    download gwd-lr

---
+++ download_gwd-lr/download.sh
@@ -0,0 +1,94 @@
+
+#~ source: http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/list_3sec.html
+
+mkdir /scratch/depfg/sutan101/gwd-lr/downloaded_on_2023-01-17/
+cd /scratch/depfg/sutan101/gwd-lr/downloaded_on_2023-01-17/
+
+#~ Effective River Width
+#~ example: wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00E000.tar.gz
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00E000.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00E060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00E120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00W060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N00W120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30E000.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30E060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30E120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30W060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30W120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N30W180.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60E000.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60E060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60E120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60W060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60W120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_N60W180.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S30E000.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S30E060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S30E120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S30W060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S30W120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S60E000.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S60E060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S60E120.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S60W060.tar.gz &
+wget http://hydro.iis.u-tokyo.ac.jp/~yamadai/GWD-LR/distribute/3sec/wth_S60W120.tar.gz &
+wait
+
+#~ Flow Direction
+#~ dir_N00E000.tar.gz
+#~ dir_N00E060.tar.gz
+#~ dir_N00E120.tar.gz
+#~ dir_N00W060.tar.gz
+#~ dir_N00W120.tar.gz
+#~ dir_N30E000.tar.gz
+#~ dir_N30E060.tar.gz
+#~ dir_N30E120.tar.gz
+#~ dir_N30W060.tar.gz
+#~ dir_N30W120.tar.gz
+#~ dir_N30W180.tar.gz
+#~ dir_N60E000.tar.gz
+#~ dir_N60E060.tar.gz
+#~ dir_N60E120.tar.gz
+#~ dir_N60W060.tar.gz
+#~ dir_N60W120.tar.gz
+#~ dir_N60W180.tar.gz
+#~ dir_S30E000.tar.gz
+#~ dir_S30E060.tar.gz
+#~ dir_S30E120.tar.gz
+#~ dir_S30W060.tar.gz
+#~ dir_S30W120.tar.gz
+#~ dir_S60E000.tar.gz
+#~ dir_S60E060.tar.gz
+#~ dir_S60E120.tar.gz
+#~ dir_S60W060.tar.gz
+#~ dir_S60W120.tar.gz
+
+#~ Drainage Area
+#~ upa_N00E000.tar.gz
+#~ upa_N00E060.tar.gz
+#~ upa_N00E120.tar.gz
+#~ upa_N00W060.tar.gz
+#~ upa_N00W120.tar.gz
+#~ upa_N30E000.tar.gz
+#~ upa_N30E060.tar.gz
+#~ upa_N30E120.tar.gz
+#~ upa_N30W060.tar.gz
+#~ upa_N30W120.tar.gz
+#~ upa_N30W180.tar.gz
+#~ upa_N60E000.tar.gz
+#~ upa_N60E060.tar.gz
+#~ upa_N60E120.tar.gz
+#~ upa_N60W060.tar.gz
+#~ upa_N60W120.tar.gz
+#~ upa_N60W180.tar.gz
+#~ upa_S30E000.tar.gz
+#~ upa_S30E060.tar.gz
+#~ upa_S30E120.tar.gz
+#~ upa_S30W060.tar.gz
+#~ upa_S30W120.tar.gz
+#~ upa_S60E000.tar.gz
+#~ upa_S60E060.tar.gz
+#~ upa_S60E120.tar.gz
+#~ upa_S60W060.tar.gz
+#~ upa_S60W120.tar.gz
