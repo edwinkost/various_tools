4b7b03fb2eb5531ce24209cc73cd4e6345f81ff5
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-02-03 15:56:28 +0100

    make jobs for the script

--- merging_netcdf_aqueduct_2021/merging_with_cdo/a_single_job_to_merge_per_gm_per_scenario.sh
+++ merging_netcdf_aqueduct_2021/merging_with_cdo/a_single_job_to_merge_per_gm_per_scenario.sh
@@ -20,18 +20,19 @@
 
 
 # load all software
+cd $HOME
 . load_anaconda_and_my_default_env.sh
 
 # go to the script folder
 cd /eejit/home/sutan101/github/edwinkost/various_tools/merging_netcdf_aqueduct_2021/merging_with_cdo/
 
 # set variables/arguments for the python script
-GCM_NAME = ${GCM_NAME}
-SCN_TYPE = ${SCN_TYPE}
-START_YR = ${START_YR}
-FINAL_YR = ${FINAL_YR}
-SRC_FOLD = "/scratch/depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16/"${GCM_NAME}"/"${SCN_TYPE}"/begin_from_/"${START_YR}"/global/netcdf/"
-OUT_FOLD = "/scratch/depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16_merged/"${GCM_NAME}"/"${SCN_TYPE}"/"
+GCM_NAME=${GCM_NAME}
+SCN_TYPE=${SCN_TYPE}
+START_YR=${START_YR}
+FINAL_YR=${FINAL_YR}
+SRC_FOLD="/scratch/depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16/"${GCM_NAME}"/"${SCN_TYPE}"/begin_from_/"${START_YR}"/global/netcdf/"
+OUT_FOLD="/scratch/depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16_merged/"${GCM_NAME}"/"${SCN_TYPE}"/"
 
 # call the script
 python cdo_merge_type ${GCM_NAME} ${SCN_TYPE} ${START_YR} ${FINAL_YR} ${SRC_FOLD} ${OUT_FOLD} 
