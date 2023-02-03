5e659b0467b9ac514f4bc2cf345730d4c3a5ff65
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-02-03 15:54:37 +0100

    make jobs for the script

--- merging_netcdf_aqueduct_2021/merging_with_cdo/all_jobs.sh
+++ merging_netcdf_aqueduct_2021/merging_with_cdo/all_jobs.sh
@@ -12,34 +12,35 @@
 #~ dr-xr-xr-x 6 sutan101 depfg 6 Jan 17 05:27 ukesm1-0-ll
 
 GCM_NAME="gfdl-esm4"
-sbatch -J gfd-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014"
-sbatch -J gfd-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100"
-sbatch -J gfd-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100"
-sbatch -J gfd-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100"
+sbatch -J gfd-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014" a_single_job_to_merge_per_gm_per_scenario.sh
+sbatch -J gfd-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+sbatch -J gfd-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+sbatch -J gfd-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
   
 #~ GCM_NAME="ipsl-cm6a-lr"
-#~ sbatch -J ips-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014"
-#~ sbatch -J ips-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J ips-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J ips-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100"
+#~ sbatch -J ips-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J ips-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J ips-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J ips-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
 
 #~ GCM_NAME="mpi-esm1-2-hr"
-#~ sbatch -J mpi-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014"
-#~ sbatch -J mpi-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J mpi-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J mpi-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100"
+#~ sbatch -J mpi-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mpi-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mpi-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mpi-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
 
 #~ GCM_NAME="mri-esm2-0"
-#~ sbatch -J mri-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014"
-#~ sbatch -J mri-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J mri-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J mri-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100"
+#~ sbatch -J mri-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mri-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mri-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J mri-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
 
 #~ GCM_NAME="ukesm1-0-ll"
-#~ sbatch -J uke-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014"
-#~ sbatch -J uke-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J uke-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100"
-#~ sbatch -J uke-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100"
+#~ sbatch -J uke-hist --export GCM_NAME=${GCM_NAME},SCN_TYPE="historical",START_YR="1960",FINAL_YR="2014" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J uke-ssp1 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp126",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J uke-ssp3 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp370",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+#~ sbatch -J uke-ssp5 --export GCM_NAME=${GCM_NAME},SCN_TYPE="ssp585",START_YR="2015",FINAL_YR="2100" a_single_job_to_merge_per_gm_per_scenario.sh
+
 
 
 #~ sutan101@node032.cluster:/scratch/depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16$ ls -lah gfdl-esm4/
